afdb2f9cff4de910c3d486cee5720d79dd2d26a4
HMarmottant <113462699+HMarmottant@users.noreply.github.com>
2023-09-06 18:53:54 +0200

    added baudrate option

--- rasp/Teansy_UART.py
+++ rasp/Teansy_UART.py
@@ -1,21 +1,22 @@
 import serial
 import serial.tools.list_ports
 
+
 class Teensy:
-    def __init__(self,vid : int , pid : int):
+    def __init__(self, vid: int, pid: int, baudrate: int = 115200):
         self._teensy = None
         for port in serial.tools.list_ports.comports():
             if port.vid == vid and port.pid == pid:
-                self._teensy = serial.Serial(port.device)
+                self._teensy = serial.Serial(port.device, baudrate=baudrate)
                 break
-        if self._teensy == None :
+        if self._teensy == None:
             raise Exception("No Device found!")
-                
-    def send_bytes(self,data : bytearray, end_bytes : bytearray = b'0xDEADBEEF') :
+
+    def send_bytes(self, data: bytearray, end_bytes: bytearray = b'0xDEADBEEF'):
         self._teensy.reset_output_buffer()
-        self._teensy.write(data.append(len(data),end_bytes))
-        while self._teensy.out_waiting :
+        self._teensy.write(data.append(len(data), end_bytes))
+        while self._teensy.out_waiting:
             pass
-        
-    def read_bytes(self,end_bytes : bytearray = b'0xDEADBEEF') -> bytearray:
+
+    def read_bytes(self, end_bytes: bytearray = b'0xDEADBEEF') -> bytearray:
         return self._teensy.read_until(end_bytes)
