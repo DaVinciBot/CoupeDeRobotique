4db7808ef43fe5586295db420583df8134f469c6
HMarmottant <113462699+HMarmottant@users.noreply.github.com>
2023-09-07 14:21:41 +0200

    Basic  Teensy Commands

--- rasp/Teansy_UART.py
+++ rasp/Teansy_UART.py
@@ -1,5 +1,12 @@
 import serial
 import serial.tools.list_ports
+import struct
+from typing import Any
+
+
+class Command:
+    GoToPoint = b"\x00"
+    SetSpeed = b"\x01"
 
 
 class Teensy:
@@ -12,11 +19,21 @@ class Teensy:
         if self._teensy == None:
             raise Exception("No Device found!")
 
-    def send_bytes(self, data: bytes, end_bytes: bytes = b'\xDE\xAD\xBE\xEF'):
+    def send_bytes(self, data: bytes, end_bytes: bytes = b'\xBA\xDD\x1C\xC5'):
         self._teensy.reset_output_buffer()
         self._teensy.write(data + bytes([len(data)]) + end_bytes)
         while self._teensy.out_waiting:
             pass
 
-    def read_bytes(self, end_bytes: bytes = b'\xDE\xAD\xBE\xEF') -> bytes:
+    def read_bytes(self, end_bytes: bytes = b'\xBA\xDD\x1C\xC5') -> bytes:
         return self._teensy.read_until(end_bytes)
+
+    def Go_To_Point(self, x: float, y: float, theta: float) -> None:
+        msg = Command.GoToPoint + struct.pack(x, "f") + struct.pack(y, "f") + struct.pack(theta, "f")
+        self.send_bytes(msg)
+    
+    def Set_Speed(self, speed : float) -> None:
+        msg = Command.GoToPoint + struct.pack(speed, "f")
+        self.send_bytes(msg)
+
+
