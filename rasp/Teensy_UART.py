b2436d9128000afa9beef4d695bf4618d390d16d
HMarmottant <113462699+HMarmottant@users.noreply.github.com>
2023-09-08 09:08:11 +0200

    ++ receiver msg type + better organisation

--- rasp/Teensy_UART.py
+++ rasp/Teensy_UART.py
@@ -4,14 +4,16 @@ import struct
 from typing import Any
 import threading
 import time
-
-
-class Command:
-    GoToPoint = b"\x00"
-    SetSpeed = b"\x01"
+import logging
 
 
 class Teensy:
+    #############
+    # Internals #
+    #############
+
+    MSG_END_BYTES = b'\xBA\xDD\x1C\xC5'
+
     def __init__(self, vid: int = 0x16C0, pid: int = 0x0483, baudrate: int = 115200):
         self._teensy = None
         for port in serial.tools.list_ports.comports():
@@ -19,20 +21,65 @@ class Teensy:
                 self._teensy = serial.Serial(port.device, baudrate=baudrate)
                 break
         if self._teensy == None:
-            raise Exception("No Device found!")
+            logging.error("No Teensy found!", stack_info=True)
         self.odometrie = [0.0, 0.0, 0.0]
         self._reciever = threading.Thread(
             target=self.__receiver__, name="TeensyReceiver")
         self._reciever.start()
 
-    def send_bytes(self, data: bytes, end_bytes: bytes = b'\xBA\xDD\x1C\xC5'):
+    def send_bytes(self, data: bytes, end_bytes: bytes = MSG_END_BYTES):
         self._teensy.reset_output_buffer()
         self._teensy.write(data + bytes([len(data)]) + end_bytes)
         while self._teensy.out_waiting:
             pass
 
-    def read_bytes(self, end_bytes: bytes = b'\xBA\xDD\x1C\xC5') -> bytes:
+    def read_bytes(self, end_bytes: bytes = MSG_END_BYTES) -> bytes:
         return self._teensy.read_until(end_bytes)
+
+    def __receiver__(self) -> None:
+        """This is started as a thread, handles the data acording to the decided format :
+
+        msg_type | msg_data | msg_length | MSG_END_BYTES
+        size : 1 | msg_length | 1 | 4
+
+        The size is in bytes.
+        It will call the corresponding function 
+        """
+        while True:
+            msg = self.read_bytes()
+            lenmsg = msg[-5]
+            if lenmsg + 5 > len(msg):
+                logging.warn(
+                    "Received Teensy message does not match declared length")
+                continue
+            try:
+                self.messagetype[msg[0]](msg[1:-5])
+            except Exception as e:
+                logging.error("Received message handling crashed \n" + str(e))
+
+    #############################
+    # Received message handling #
+    #############################
+
+    def rcv_odometrie(self, msg: bytes):
+        self.odometrie = [struct.unpack("<f", msg[0:4]),
+                          struct.unpack("<f", msg[4:8]),
+                          struct.unpack("<f", msg[8:12])]
+
+    """
+    This is used to match a handling function to a message type.
+    """
+    messagetype = {
+        b'\x80': rcv_odometrie
+    }
+
+    #########################
+    # User facing functions #
+    #########################
+
+    class Command:
+        GoToPoint = b"\x00"
+        SetSpeed = b"\x01"
 
     def Go_To(self, x: float, y: float, direction: bool = False, speed: bytes = b'\xFF', next_position_delay: int = 100, action_error_auth: int = 20, traj_precision: int = 50) -> None:
         """Got to a point
@@ -45,7 +92,7 @@ class Teensy:
             action_error_auth (int, optional): _description_. Defaults to 20.
             traj_precision (int, optional): _description_. Defaults to 50.
         """
-        msg = Command.GoToPoint + \
+        msg = self.Command.GoToPoint + \
             struct.pack("<f", x) + \
             struct.pack("<f", y) + \
             struct.pack("<?", direction) + \
@@ -58,15 +105,5 @@ class Teensy:
         self.send_bytes(msg)
 
     def Set_Speed(self, speed: float) -> None:
-        msg = Command.GoToPoint + struct.pack(speed, "f")
+        msg = self.Command.GoToPoint + struct.pack(speed, "<f")
         self.send_bytes(msg)
-
-    def __receiver__(self) -> None:
-        while True:
-            msg = self.read_bytes()
-            # print(msg.hex(sep="|"))
-            # print(msg[-5])
-            msg = msg[:-5]
-            self.odometrie = [struct.unpack("f", msg[1:5]),
-                              struct.unpack("f", msg[5:9]),
-                              struct.unpack("f", msg[9:13])]            # time.sleep(0.1)
