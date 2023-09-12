8aafa791894718a2e3254d992922af58424991b2
HMarmottant <113462699+HMarmottant@users.noreply.github.com>
2023-09-12 22:18:50 +0200

    added crc to send bytes

--- rasp/Libraries/Teensy_UART.py
+++ rasp/Libraries/Teensy_UART.py
@@ -8,11 +8,11 @@ import crc8
 
 
 class Teensy():
-    def __init__(self, vid: int = 0x16C0, pid: int = 0x0483, baudrate: int = 115200, crc : bool = True):
+    def __init__(self, vid: int = 0x16C0, pid: int = 0x0483, baudrate: int = 115200, crc: bool = True):
         self._teensy = None
         self.crc = crc
         self._crc8 = crc8.crc8()
-        
+
         for port in serial.tools.list_ports.comports():
             if port.vid == vid and port.pid == pid:
                 self._teensy = serial.Serial(port.device, baudrate=baudrate)
@@ -27,7 +27,12 @@ class Teensy():
 
     def send_bytes(self, data: bytes, end_bytes: bytes = b'\xBA\xDD\x1C\xC5'):
         self._teensy.reset_output_buffer()
-        self._teensy.write(data + bytes([len(data)]) + end_bytes)
+        msg = data + bytes([len(data)])
+        if self.crc:
+            self._crc8.update(msg)
+            msg += self._crc8.digest()
+
+        self._teensy.write(msg + end_bytes)
         while self._teensy.out_waiting:
             pass
 
@@ -48,19 +53,19 @@ class Teensy():
         """
         while True:
             msg = self.read_bytes()
-            
-            if(self.crc) :
+
+            if (self.crc):
                 crc = msg[-5:-5]
                 msg = msg[:-5]
                 self._crc8.update(msg)
-                if( self._crc8.digest() != crc):
+                if (self._crc8.digest() != crc):
                     logging.warn(
                         "Inivalid CRC8 skipping message"
                     )
                 self._crc8.reset()
-            else :
+            else:
                 msg = msg[:-4]
-            
+
             lenmsg = msg[:-1]
             if lenmsg + 5 > len(msg):
                 logging.warn(
