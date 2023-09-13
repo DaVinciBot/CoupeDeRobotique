8c06d90c10fb8d7500b6d5d76226b26be765cb7a
HMarmottant <113462699+HMarmottant@users.noreply.github.com>
2023-09-13 10:44:57 +0200

    better interface and redid library

--- rasp/Interface.py
+++ rasp/Interface.py
@@ -1,14 +1,21 @@
-from Libraries.Rolling_Basis import Rolling_basis 
+from Libraries.Teensy_Com import Rolling_basis
 import pywebio
 import time
+import threading
 
 
 def app():
     global teensy
     old = None
-    pywebio.output.put_button("set_coord",
-                              set_coords
-                              )
+    th = threading.Thread(target=pin)
+    pywebio.session.register_thread(th)
+    th.start()
+    pywebio.output.put_scope("buttons")
+    pywebio.output.put_buttons(
+        ["Home", "dummy"],
+        onclick=buttons,
+        scope="buttons"
+    )
     pywebio.output.put_scope("data")
     pywebio.output.put_table(teensy.odometrie, scope="data")
     while (True):
@@ -19,19 +26,37 @@ def app():
             old = teensy.odometrie
 
 
-def set_coords():
-    coords = pywebio.input.input_group("Coordinates",
-                                       [
-                                           pywebio.input.input(
-                                               "x", name="x", type=pywebio.input.FLOAT),
-                                           pywebio.input.input(
-                                               "y", name="y", type=pywebio.input.FLOAT),
-                                       ],
-                                       )
-    teensy.Go_To(coords["x"], coords["y"])
+def pin():
+    while True:
+        coords = pywebio.input.input_group("Coordinates",
+                                           [
+                                               pywebio.input.input(
+                                                   "x", name="x", type=pywebio.input.FLOAT),
+                                               pywebio.input.input(
+                                                   "y", name="y", type=pywebio.input.FLOAT),
+                                           ],
+                                           )
+        teensy.Go_To(coords["x"], coords["y"])
+
+
+def buttons(button):
+    match button:
+        case "Home":
+            teensy.Home_Position()
+
+
+class dummy:
+    odometrie = [[0, 0, 0]]
+
+    def Go_To(self, x, y):
+        print("going to ", x, " ", y)
+
+    def Home_Position(self):
+        print("homing")
 
 
 if __name__ == "__main__":
     global teensy
-    teensy = Rolling_basis()
+    # teensy = Rolling_basis()
+    teensy = dummy()
     pywebio.start_server(app, port=42069)
---
+++ rasp/Libraries/Teensy_Com.py
@@ -0,0 +1,154 @@
+# autopep8: off
+# import os 
+# import sys
+# sys.path.append(os.getcwd())
+# autopep8: on
+
+import serial
+import serial.tools.list_ports
+from typing import Any, Callable
+import threading
+import time
+import logging
+import crc8
+import struct
+
+
+class Teensy():
+    def __init__(self, vid: int = 0x16C0, pid: int = 0x0483, baudrate: int = 115200, crc: bool = True):
+        self._teensy = None
+        self.crc = crc
+        self._crc8 = crc8.crc8()
+
+        for port in serial.tools.list_ports.comports():
+            if port.vid == vid and port.pid == pid:
+                self._teensy = serial.Serial(port.device, baudrate=baudrate)
+                break
+        if self._teensy == None:
+            raise Exception("No Device found!")
+        self.messagetype = {}
+        self._reciever = threading.Thread(
+            target=self.__receiver__, name="TeensyReceiver")
+        self._reciever.start()
+
+    def send_bytes(self, data: bytes, end_bytes: bytes = b'\xBA\xDD\x1C\xC5'):
+        self._teensy.reset_output_buffer()
+        msg = data + bytes([len(data)])
+        if self.crc:
+            self._crc8.update(msg)
+            msg += self._crc8.digest()
+
+        self._teensy.write(msg + end_bytes)
+        while self._teensy.out_waiting:
+            pass
+
+    def read_bytes(self, end_bytes: bytes = b'\xBA\xDD\x1C\xC5') -> bytes:
+        return self._teensy.read_until(end_bytes)
+
+    def add_callback(self, func: Callable[[bytes], None], id: int):
+        self.messagetype[id] = func
+
+    def __receiver__(self) -> None:
+        """This is started as a thread, handles the data acording to the decided format :
+
+        msg_type | msg_data | msg_length | CRC8 (optional) |MSG_END_BYTES
+        size : 1 | msg_length | 1 | 1 | 4
+
+        The size is in bytes.
+        It will call the corresponding function 
+        """
+        while True:
+            msg = self.read_bytes()
+
+            if (self.crc):
+                crc = msg[-5:-5]
+                msg = msg[:-5]
+                self._crc8.update(msg)
+                if (self._crc8.digest() != crc):
+                    logging.warn(
+                        "Inivalid CRC8 skipping message"
+                    )
+                self._crc8.reset()
+            else:
+                msg = msg[:-4]
+
+            lenmsg = msg[:-1]
+            if lenmsg + 5 > len(msg):
+                logging.warn(
+                    "Received Teensy message does not match declared length")
+                continue
+            try:
+                self.messagetype[msg[0]](msg=msg[1:-6])
+            except Exception as e:
+                logging.error("Received message handling crashed :\n" + e.args)
+                time.sleep(0.5)
+
+
+class Rolling_basis(Teensy):
+    ######################
+    # Rolling basis init #
+    ######################
+
+    def __init__(self, vid: int = 5824, pid: int = 1155, baudrate: int = 115200, crc: bool = True):
+        super().__init__(vid, pid, baudrate, crc)
+        self.odometrie = [[0.0, 0.0, 0.0]]
+        self.action_finished = False
+        """
+        This is used to match a handling function to a message type.
+        add_callback can also be used.
+        """
+        self.messagetype = {
+            128: self.rcv_odometrie,  # \x80
+            69: self.rcv_action_finish  # \x45
+        }
+
+    #############################
+    # Received message handling #
+    #############################
+
+    def rcv_odometrie(self, msg: bytes):
+        self.odometrie = [struct.unpack("<f", msg[0:4]),
+                          struct.unpack("<f", msg[4:8]),
+                          struct.unpack("<f", msg[8:12])]
+
+    def rcv_action_finish(self, msg: bytes):
+        self.action_finished = True
+
+    #########################
+    # User facing functions #
+    #########################
+
+    class Command:
+        GoToPoint = b"\x00"
+        SetSpeed = b"\x01"
+
+    def Go_To(self, x: float, y: float, direction: bool = False, speed: bytes = b'\x64', next_position_delay: int = 100, action_error_auth: int = 20, traj_precision: int = 50) -> None:
+        """Got to a point
+
+        Args:
+            x (float): x coordinate
+            y (float): y coordinate
+            direction (bool, optional): whether to go backwards or forwards. Defaults to False.
+            next_position_delay (int, optional): _description_. Defaults to 100.
+            action_error_auth (int, optional): _description_. Defaults to 20.
+            traj_precision (int, optional): _description_. Defaults to 50.
+        """
+        msg = self.Command.GoToPoint + \
+            struct.pack("<f", x) + \
+            struct.pack("<f", y) + \
+            struct.pack("<?", direction) + \
+            speed + \
+            struct.pack("<H", next_position_delay) + \
+            struct.pack("<H", action_error_auth) + \
+            struct.pack("<H", traj_precision)
+        # https://docs.python.org/3/library/struct.html#format-characters
+
+        self.send_bytes(msg)
+        self.action_finished = True
+
+    def Set_Speed(self, speed: float) -> None:
+        msg = self.Command.GoToPoint + struct.pack(speed, "f")
+        self.send_bytes(msg)
+
+    def Home_Position(self):
+        print("homing")
