544928d34f3073943f185657d726d3a8b43c179e
Urbain <urbain.lantres@tutanota.com>
2023-10-11 18:24:29 +0200

    Update api.py
    should be fine with async now

--- rasp/bot/api.py
+++ rasp/bot/api.py
@@ -6,15 +6,21 @@ PORT = 3000
 uri = f"ws://{IP}:{PORT}"
 
 def update_lidar(data: list[float]):
-    with websockets.connect(uri+"/lidar") as websocket:
-        data = list(map(str, data))
-        data = "[" + ",".join(data) + "]"
-        asyncio.run(websocket.send("set:"+data))
-
+    asyncio.run(async_update_lidar(data))
 
 def update_log(data: str):
-    with websockets.connect(uri+"/log") as websocket:
-        asyncio.run(websocket.send("update:["+data+"]"))
+    asyncio.run(update_log(data))
+
+async def async_update_lidar(data: list[float]):
+    async with websockets.connect(uri+"/lidar") as websocket:
+        data = list(map(str, data))
+        data = "[" + ",".join(data) + "]"
+        await websocket.send("set:"+data)
+
+
+async def update_log(data: str):
+    async with websockets.connect(uri+"/log") as websocket:
+        await websocket.send("update:["+data+"]")
 
 
 
