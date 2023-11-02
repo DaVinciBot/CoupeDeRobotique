249ef69d18ca06b63646992912b68b79f64e4b53
Urbain <urbain.lantres@tutanota.com>
2023-11-02 20:00:02 +0100

    Cleanup

--- rasp/classes/test_lidar.py
+++
@@ -1,6 +0,0 @@
-import lidar as l
-import math
-
-lidar = l.Lidar(-math.pi, math.pi)
-print(lidar.safe_get_nearest_point_between(90,180,1/3))
-print(f"is obstacle in front : {lidar.is_obstacle_infront()}")
