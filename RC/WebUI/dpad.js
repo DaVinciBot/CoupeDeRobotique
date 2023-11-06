ec4b887d8454882b24ef9f02eef8bcffdce47976
Urbain <urbain.lantres@tutanota.com>
2023-11-06 23:00:42 +0100

    New console font + websocket link for dpad

--- RC/WebUI/dpad.js
+++ RC/WebUI/dpad.js
@@ -3,21 +3,23 @@ let down = document.querySelector(".down");
 let left = document.querySelector(".left");
 let right = document.querySelector(".right");
 
+let robot = new WebSocket("ws://127.0.0.1:3000/cmd");
+
+
 let dpad_divs = [...up.children,...down.children,...left.children,...right.children];
 for (let i = 0; i < dpad_divs.length; i++) {
     dpad_divs[i].addEventListener("click", function (event) {
         let direction = event.target.parentElement.className;
-        let msg = direction + "$=$" + 100;
-        console.log(msg);
-    });
-    dpad_divs[i].addEventListener("mousedown", function (event) {
-        let direction = event.target.parentElement.className;
-        let msg = direction + "$=$" ;
-        console.log(msg);
-    });
-    dpad_divs[i].addEventListener("mouseup", function (event) {
-        let direction = event.target.parentElement.className;
-        let msg = direction + "$=$" + 0;
-        console.log(msg);
+        let msg = "";
+        if (direction == "up") {
+            msg = "[10,0,0]";
+        } else if (direction = "down") {
+            msg = "[-10,0,0]";
+        } else if (direction = "left") {
+            msg = "[0,10,0]";
+        } else if (direction = "right") {
+            msg = "[0,-10,0]";
+        }
+        robot.send("set$=$goto"+msg);
     });
 }
