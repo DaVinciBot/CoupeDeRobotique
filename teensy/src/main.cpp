180cd2d5b1ffb33932db59aed9c30e5a9a3c3972
Urbain <urbain.lantres@tutanota.com>
2023-12-06 10:49:33 +0100

    Update cooldown
    Retard juste la déconnexion

--- teensy/src/main.cpp
+++ teensy/src/main.cpp
@@ -47,6 +47,9 @@
 #define R_IN2 1
 #define R_IN1 0
 
+// Slow communication to avoid closing of serial port on the computer
+#define ODO_COOLDOWN 1
+
 Rolling_Basis *rolling_basis_ptr = new Rolling_Basis(ENCODER_RESOLUTION, CENTER_DISTANCE, WHEEL_DIAMETER);
 
 Rolling_Basis_Params rolling_basis_params{
@@ -266,11 +269,11 @@ void loop()
     pos_msg.x = rolling_basis_ptr->X;
     pos_msg.y = rolling_basis_ptr->Y;
     pos_msg.theta = rolling_basis_ptr->THETA;
-    if (cooldown > 10){
+    if (cooldown > ODO_COOLDOWN){
       com->send_msg((byte *)&pos_msg, sizeof(msg_Update_Position));
       cooldown = 0;
     }
-    else
+    else 
       cooldown++;
     counter = 0;
   }
