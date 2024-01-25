949bacde9131952cee100b85a64fdf37da304423
UrbsKali <urbain.lantres@tutanota.com>
2024-01-25 17:05:51 +0100

    Delete rasp/main_test.py

--- rasp/main_test.py
+++
@@ -1,8 +0,0 @@
-from bot import RollingBasis, Shapes
-
-bot = RollingBasis()
-#bot.curve_go_to(Shapes.OrientedPoint(20,0,0), 10, 20, skip_queue=True)
-#bot.Go_To(Shapes.OrientedPoint(0,0,0),skip_queue=True)
-#bot.Go_To(Shapes.OrientedPoint(30,0,0))
-#bot.Go_To(Shapes.OrientedPoint(20,20,0))
-#bot.Go_To(Shapes.OrientedPoint(0,10,0))
