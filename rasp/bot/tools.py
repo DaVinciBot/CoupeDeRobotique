3e39edde80a10e241c24aa0797cbab82373d2e25
RomainCuchet <romain.cuchet02@gmail.com>
2024-01-25 12:21:52 +0100

    sucessfully implemented compute_go_to_destination()

--- rasp/bot/tools.py
+++ rasp/bot/tools.py
@@ -1,7 +1,7 @@
 from bot.Shapes import Point,OrientedPoint,Rectangle,Circle
 from typing import Union
 
-def compute_go_to_destination(actual_position : OrientedPoint, __object : object,  distance = 0, nb_digits : int = 0)->Union[OrientedPoint,bool]:
+def compute_go_to_destination(actual_position : OrientedPoint, __object : object,  distance = 0, nb_digits : int = 2, closer = True)->Union[OrientedPoint,bool]:
     def compute_detination(destination_point : OrientedPoint):
         if(destination_point.y == actual_position.y):
             if destination_point.x>actual_position.x: # compute wether the starting point is on the  bottom or on the top of the robot
@@ -13,13 +13,24 @@ def compute_go_to_destination(actual_position : OrientedPoint, __object : object
             return destination_point+OrientedPoint(0,distance)
         a = (destination_point.y - actual_position.y) / (destination_point.x - actual_position.x)
         b = destination_point.y - a *destination_point.x
-        delta = 4 * (a**2) * (b**2) - 4 * (1 + a**2) * (b**2 - distance**2)
+        c = 1+a**2
+        d = 2*(-destination_point.x+a*(b-destination_point.y))
+        e = destination_point.x**2+(b-destination_point.y)**2-distance**2
+        delta = d**2-4*c*e
         if delta<0:
             return False
-        p1 = (-2*a*b-delta)**(1/2)/(2*(1+a**2))
-        p2 = (-2*a*b-delta)**(1/2)/(2*(1+a**2))
-        if destination_point.get_distance(p1)<= destination_point.get_distance(p2): return p1
-        return p2
+        x1 = (-d-delta**(1/2))/(2*c)
+        x2 = (-d+delta**(1/2))/(2*c)
+        p1 = OrientedPoint(x1,a*x1+b)
+        p2 = OrientedPoint(x2,a*x2+b)
+        if closer:
+            if actual_position.get_distance(p1) < actual_position.get_distance(p2): return p1
+            return p2
+        else:
+            if actual_position.get_distance(p1) > actual_position.get_distance(p2): return p1
+            return p2
+        
+        
     if isinstance(__object, (Rectangle,Circle)):
         if distance == 0: return __object.center
         arrival = compute_detination(__object.center)
--- rasp/tests/test_tools.py
+++ rasp/tests/test_tools.py
@@ -37,14 +37,23 @@ class TestCompute_go_to:
         assert Point(1,2) == compute_go_to_destination(p4,r,distance=1) # from the top 
         
     def test_default(self):
-        r = Rectangle(Point(7,1),Point(5,3))
-        c = Circle(Point(6,2),3)
-        p1 = OrientedPoint(1,7)
-        p2 = OrientedPoint(6,2)
-        p3 = OrientedPoint(5,3)
-        assert p2 == compute_go_to_destination(p1,p2)
-        print(compute_go_to_destination(p1,p2,distance=3*2**(1/2),nb_digits=2))
-        assert p3 == compute_go_to_destination(p1,p2,distance=3*2**(1/2),nb_digits=2)
+        a = Point(2,2)
+        p1 = Point(0,0)
+        p2 = Point(0,4)
+        p3 = Point(4,0)
+        p4 = Point(4,4)
+        q1 = Point(1,1)
+        q2 = Point(1,3)
+        q3 = Point(3,1)
+        q4 = Point(3,3)
+        assert compute_go_to_destination(p1,a,distance=2**(1/2),nb_digits=1) == q1
+        assert compute_go_to_destination(p1,a,distance=2**(1/2),nb_digits=1, closer=False) == q4
+        assert compute_go_to_destination(p2,a,distance=2**(1/2),nb_digits=1) == q2
+        assert compute_go_to_destination(p2,a,distance=2**(1/2),nb_digits=1, closer=False) == q3
+        assert compute_go_to_destination(p3,a,distance=2**(1/2),nb_digits=1) == q3
+        assert compute_go_to_destination(p3,a,distance=2**(1/2),nb_digits=1, closer=False) == q2
+        assert compute_go_to_destination(p4,a,distance=2**(1/2),nb_digits=1) == q4
+        assert compute_go_to_destination(p4,a,distance=2**(1/2),nb_digits=1, closer=False) == q1
         
         
         
