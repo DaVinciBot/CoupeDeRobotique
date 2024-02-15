909f11ebc3c07fc8456cd9174c50cc3e0abd2fdc
RomainCuchet <romain.cuchet02@gmail.com>
2024-02-15 15:02:08 +0100

    debug

--- rasp/test.py
+++ rasp/test.py
@@ -10,18 +10,19 @@ def go_to(__object : object,  distance = 0, nb_digits : int = 2, closer = True)-
     destination_point = compute_go_to_destination(rolling_basis.odometrie,__object,distance,nb_digits=nb_digits,closer=closer)
     if isinstance(destination_point,OrientedPoint):
         if State.go_to_verif:
-            if arena.enable_go_to():
+            if arena.enable_go_to(rolling_basis.odometrie,destination_point):
+                print(destination_point)
                 rolling_basis.Go_To(destination_point)
                 return True
             return False
         rolling_basis.Go_To(destination_point)
+        print(destination_point)
         return True
     return False
-r = Rectangle(Point(2,2),Point(4,4))
+r = Rectangle(Point(10,0),Point(0,10))
 lidar = Lidar()
 arena = MarsArena(0)
 l = Logger()
 rolling_basis = RollingBasis()
-actuators = Actuators()
-rolling_basis.Set_Home()
 go_to(r)
+go_to(Point(10,0))
