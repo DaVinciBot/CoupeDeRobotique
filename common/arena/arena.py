7730439782e24d93bbd83b7c20880cf92b96fa60
AdrienB <56419448+DaAdri@users.noreply.github.com>
2024-03-12 19:21:59 +0100

    Make Go_To use Point instead of OrientedPoint Add alternative OrientedPoint constructor from_Point
    (cherry picked from commit fdc05fecfacd2d936921d98a6a989ffff130ca43)

--- common/arena/arena.py
+++ common/arena/arena.py
@@ -121,7 +121,7 @@ class Arena:
         ):
             return None
 
-        # return closest or furthest intersection
+        # Return closest or furthest intersection
 
         else:
             if distance(start_point, intersections.geoms[0]) <= distance(
