ddf7c993db2172a4c8ba3e660ebcb4011b0c3b4e
AdrienB <56419448+DaAdri@users.noreply.github.com>
2024-05-10 15:34:49 +0200

    Log

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -567,12 +567,10 @@ class MainBrain(Brain):
     @Brain.task(process=False, run_on_start=False, timeout=30)
     async def solar_panels_stage(self) -> None:
         print(
-            Point(
-                CONFIG.START_INFO_BY_TEAM[self.team]["start_x"],
-                (
+
                     max(self.arena.solar_panels_y)
                     + (1.0 if self.team == "y" else -1.0) * 10.0,
-                ),
+
             )
         )
         asyncio.create_task(self.control_solar_panels())
