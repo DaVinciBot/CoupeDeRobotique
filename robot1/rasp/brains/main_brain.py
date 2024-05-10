7d0ab372081da6b786ce76f66acb1ff7a0c083a0
AdrienB <56419448+DaAdri@users.noreply.github.com>
2024-05-10 16:40:23 +0200

    Fix solar panels distance for blue

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -579,20 +579,16 @@ class MainBrain(Brain):
 
     @Brain.task(process=False, run_on_start=False, timeout=30)
     async def solar_panels_stage(self) -> None:
-        print(
-            Point(
-                CONFIG.START_INFO_BY_TEAM[self.team]["start_x"],
-                max(self.arena.solar_panels_y)
-                + (1.0 if self.team == "y" else -1.0) * 10.0,
-            )
-        )
+
         asyncio.create_task(self.control_solar_panels())
+        target_y = (
+            (max(self.arena.solar_panels_y) + 10.0)
+            if self.team == "y"
+            else (min(self.arena.solar_panels_y) - 10.0)
+        )
+
         go_to_result = await self.rolling_basis.go_to_and_wait(
-            Point(
-                CONFIG.START_INFO_BY_TEAM[self.team]["start_x"],
-                max(self.arena.solar_panels_y)
-                + (1.0 if self.team == "y" else -1.0) * 10.0,
-            ),
+            Point(CONFIG.START_INFO_BY_TEAM[self.team]["start_x"], target_y),
             timeout=30.0,
             **CONFIG.GO_TO_PROFILES["slow_and_precise"],
         )
