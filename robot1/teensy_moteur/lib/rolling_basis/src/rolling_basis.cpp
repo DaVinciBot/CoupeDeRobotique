7b0a926bed549a9ba5dbf39d4d9a1888852d7b70
Florian-BARRE <flo.others@gmail.com>
2024-05-22 23:21:30 +0200

    fix parameter name error

--- robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
+++ robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
@@ -36,12 +36,12 @@ Rolling_Basis::Rolling_Basis(unsigned short encoder_resolution, float center_dis
 
 // Methods
 // Inits function
-void Rolling_Basis::init_right_motor(byte enca, byte encb, byte pwm, byte in2, byte in1, float kp, float kd, float ki, float correction_factor = 1.0, byte threshold_pwm_value = 0)
+void Rolling_Basis::init_right_motor(byte in2, byte in1, byte pwm, byte enca, byte encb, float kp, float kd, float ki, float correction_factor = 1.0, byte threshold_pwm_value = 0)
 {
     this->right_motor = new Motor(enca, encb, pwm, in2, in1, kp, kd, ki, correction_factor, threshold_pwm_value);
 }
 
-void Rolling_Basis::init_left_motor(byte enca, byte encb, byte pwm, byte in2, byte in1, float kp, float kd, float ki, float correction_factor = 1.0, byte threshold_pwm_value = 0)
+void Rolling_Basis::init_left_motor(byte in2, byte in1, byte pwm, byte enca, byte encb, float kp, float kd, float ki, float correction_factor = 1.0, byte threshold_pwm_value = 0)
 {
     this->left_motor = new Motor(enca, encb, pwm, in2, in1, kp, kd, ki, correction_factor, threshold_pwm_value);
 }
