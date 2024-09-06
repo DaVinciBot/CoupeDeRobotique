4d88fb9a5cf7e03067ffa7ed21939af48f683e04
= <romain.cuchet02@gmail.com>
2024-09-06 10:51:49 +0700

    Refactor actuator initialization and usage in main.cpp

--- robot1/teensy_actuator/src/main.cpp
+++ robot1/teensy_actuator/src/main.cpp
@@ -4,46 +4,31 @@
 #include <Servo.h>
 #include <Bonezegei_A4988.h>
 
+
 Com *com;
-Servo *servos[48] = {nullptr};                // higher than the maximum number of pin, track using pin
-Bonezegei_A4988 *steppers[48] = {nullptr};    // higher than the maximum number of pin, track using motor_pin_1
+
+void* actuators[48] = {nullptr};
 LiquidCrystal_I2C *lcd = nullptr;      // higher than the maximum number of pin, track using pin
 void (*functions[256])(byte *msg, byte size); // a tab a pointer to void functions
-
-// Define a global array of Servo_Motor. Some name of variables are not allowed becaused they are used in Servo
-
-bool is_servo_declared(int i)
-{
-  if (servos[i] == nullptr)
-    return false;
-  return true;
-}
-
-bool is_stepper_declared(int i)
-{
-  if (steppers[i] == nullptr)
-    return false;
-  return true;
-}
 
 void call_servo_go_to(byte *msg, byte size)
 {
   msg_Servo_Go_To *servo_go_to_msg = (msg_Servo_Go_To *)msg;
-  if (!is_servo_declared(servo_go_to_msg->pin))
+  if (actuators[servo_go_to_msg->pin]==nullptr)
   {
-    Servo *actuator = new Servo();
-    actuator->attach(servo_go_to_msg->pin);
-    servos[servo_go_to_msg->pin] = actuator;
+    Servo *servo = new Servo();
+    servo->attach(servo_go_to_msg->pin);
+    servos[servo_go_to_msg->pin] = (void*)servo;
   }
-  servo_go_to(servos[servo_go_to_msg->pin], servo_go_to_msg->angle);
+  servo_go_to((Servo*)servos[servo_go_to_msg->pin], servo_go_to_msg->angle);
 }
 
 void call_servo_go_to_detach(byte *msg, byte size)
 {
   msg_Servo_Go_To_Detach *servo_go_to_msg = (msg_Servo_Go_To_Detach *)msg;
-  Servo *actuator = new Servo();
-  actuator->attach(servo_go_to_msg->pin);
-  servos[servo_go_to_msg->pin] = actuator;
+  Servo *servo = new Servo();
+  servo->attach(servo_go_to_msg->pin);
+  servos[servo_go_to_msg->pin] = servo;
   servo_go_to(servos[servo_go_to_msg->pin], servo_go_to_msg->angle);
   delay(servo_go_to_msg->detach_delay);
   servos[servo_go_to_msg->pin]->detach();
@@ -52,21 +37,24 @@ void call_servo_go_to_detach(byte *msg, byte size)
 void call_stepper_step(byte *msg, byte size)
 {
   msg_Stepper_Go_To *stepper_go_to_msg = (msg_Stepper_Go_To *)msg;
-  if (!is_stepper_declared(stepper_go_to_msg->pin_dir))
+  if (servos[stepper_go_to_msg->pin_dir] == nullptr)
   {
     Bonezegei_A4988 *stepper = new Bonezegei_A4988(
-        stepper_go_to_msg->pin_dir,
-        stepper_go_to_msg->pin_step);
+      stepper_go_to_msg->pin_dir,
+      stepper_go_to_msg->pin_step
+      );
     stepper->begin();
-    steppers[stepper_go_to_msg->pin_dir] = stepper;
+    actuators[stepper_go_to_msg->pin_dir] = (void*)stepper;
     pinMode(stepper_go_to_msg->pin_driver, OUTPUT);
   }
-  steppers[stepper_go_to_msg->pin_dir]->setSpeed(stepper_go_to_msg->speed);
+  Bonezegei_A4988* stepper = (Bonezegei_A4988*)actuators[stepper_go_to_msg->pin_dir];
+  stepper->setSpeed(stepper_go_to_msg->speed);
   stepper_step(
-      steppers[stepper_go_to_msg->pin_dir],
-      stepper_go_to_msg->steps,
-      stepper_go_to_msg->dir,
-      stepper_go_to_msg->pin_driver);
+    stepper,
+    stepper_go_to_msg->steps,
+    stepper_go_to_msg->dir,
+    stepper_go_to_msg->pin_driver
+  );
 }
 
 void lcd_init(byte *msg, byte size)
@@ -99,17 +87,17 @@ void setup()
 
 void loop()
 {
-
-  // Com
   handle_callback(com);
 }
 
 /*
 
- This code was realized by Florian BARRE
-    ____ __
-   / __// /___<
-  / _/ / // _ \
- /_/  /_/ \___/
+ This code was realized by Romain CUCHET
+__________                      .__         _________  
+\______   \ ____   _____ _____  |__| ____   \_   ___ \ 
+ |       _//  _ \ /     \\__  \ |  |/    \  /    \  \/ 
+ |    |   (  <_> )  Y Y  \/ __ \|  |   |  \ \     \____
+ |____|_  /\____/|__|_|  (____  /__|___|  /  \______  /
+        \/             \/     \/        \/          \/ 
 
 */
