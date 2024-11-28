45421d224bcd2a0688b793efc879cb827f12658b
Florian-BARRE <flo.others@gmail.com>
2024-11-28 16:25:39 +0100

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -31,20 +31,34 @@ class MainBrain(Brain):
         self.rolling_basis: RollingBasis
         super().__init__(logger, self)
 
-    @Brain.task(process=False, run_on_start=True, refresh_rate=1)
+    @Brain.task(process=False, run_on_start=True, refresh_rate=0.5)
     async def rolling_basis_state(self):
         """
         Get the state of the rolling basis
         """
-        print(
+        self.logger.log(
             f"Rolling basis state:\n"
             f"Odometrie: {self.rolling_basis.odometrie}\n"
             f"Linear Speed: {self.rolling_basis.linear_speed}\n"
-            f"Angular Speed:{self.rolling_basis.angular_speed}"
+            f"Angular Speed:{self.rolling_basis.angular_speed}\n\n",
+            LogLevels.DEBUG
         )
-        # self.rolling_basis.set_speed_and_position(
-        #     target_linear_speed=0.0,
-        #     target_angular_speed=0.0,
-        #     target_position=OrientedPoint((0.0, 0.0), 0.0)
-        # )
+        
+    @Brain.task(process=False, run_on_start=True)
+    async def drive_rob(self):
+        """
+        Get the state of the rolling basis
+        """
+        self.logger.log("Driving the robot...", LogLevels.DEBUG)
+        await asyncio.sleep(1)
+        self.logger.log("GO !", LogLevels.DEBUG)
+        
+        self.rolling_basis.set_speed_and_position(
+            target_linear_speed = 1.0,
+            target_angular_speed = 0.0,
+            target_position = OrientedPoint((1.0, 0.0), 0.0)
+        )
+        
+    
+
         
