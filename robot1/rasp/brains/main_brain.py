f35afc8bfc17ee7df80216e2c457767fd7e2663a
Florian-BARRE <flo.others@gmail.com>
2024-11-28 14:45:21 +0100

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -33,7 +33,7 @@ class MainBrain(Brain):
 
 
     @Brain.task(process=False, run_on_start=True, refresh_rate=1)
-    def rolling_basis_state(self):
+    await def rolling_basis_state(self):
         """
         Get the state of the rolling basis
         """
