bbf7847e209a8fc76aa0a94af0268d313761f81e
Austin <eliottroussille@gmail.com>
2024-12-12 16:51:59 +0100

    Fix curve calcul

--- common/deplacement_supervisor/curve.py
+++ common/deplacement_supervisor/curve.py
@@ -2,6 +2,9 @@ class Curve:
 	def __init__(self, departure_speed: float, max_speed: float, arrival_speed: float, total_distance: float, acceleration_max: float, deceleration_max: float) -> None:
 		# We split the movement in 3 phases: departure, max speed, arrival
 
+		self.Cd = acceleration_max  # Departure phase slope
+		self.Ca = deceleration_max  # Arrival phase slope
+
 		self.departure_speed = departure_speed
 		self.mid_speed = max_speed
 		self.arrival_speed = arrival_speed
@@ -11,29 +14,25 @@ class Curve:
 		self.arrival_distance = (max_speed ** 2 - arrival_speed ** 2) / (2 * deceleration_max)  # Arrival total_distance
 		self.mid_distance = self.total_distance - self.departure_distance - self.arrival_distance
 
-		self.departure_time = 2 * self.departure_distance / (self.departure_speed + self.mid_speed)  # total time of departure phase
-		self.arrival_time = 2 * self.arrival_distance / (self.arrival_speed + self.mid_speed)  # total time of arrival phase
+		self.departure_time = (self.mid_speed - self.departure_speed) / self.Cd  # total time of departure phase
 		self.mid_time = self.mid_distance / self.mid_speed
+		self.arrival_time = (self.mid_speed - self.arrival_speed) / self.Ca  # total time of arrival phase
 
 		self.total_time = self.departure_time + self.mid_time + self.arrival_time
 
-		self.Cd = acceleration_max  # Departure phase slope
-		self.Ca = deceleration_max  # Arrival phase slope
+		#TODO: Si pas le temps de monter à Vmax, big bug, quoique peut-être réglé depuis Planned Velocity en min
 
 	def V1(self,t):
 		return self.departure_speed + self.Cd * t  # Velocity on departure phase equation
 	def V2(self,t):
 		return self.mid_speed  # Velocity on max speed phase equation
 	def V3(self,t):
-		return self.arrival_speed + self.Ca * (t - self.departure_time - self.mid_time)  # Velocity on arrival phase equation
+		return self.arrival_speed - self.Ca * (t - self.total_time)  # Velocity on arrival phase equation
 
 	def PlannedVelocity(self, t):
-		if t <= self.departure_time:
-			return self.V1(t)
-		elif t <= self.departure_time + self.mid_time:
-			return self.V2(t)
-		elif t <= self.total_time:
-			return self.V3(t)
+		if t<self.total_time:
+			Vitesse=[self.V1(t), self.V2(t), self.V3(t)]
+			return min([i for i in Vitesse if i>=0])
 		else:
 			return self.arrival_speed
 
