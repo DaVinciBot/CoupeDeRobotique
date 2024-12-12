f47cf76276309a975e2187db987fdffc2c13793d
Austin <eliottroussille@gmail.com>
2024-12-12 16:52:22 +0100

    Fix linear and angular speed

--- common/deplacement_supervisor/supervisor.py
+++ common/deplacement_supervisor/supervisor.py
@@ -1,3 +1,5 @@
+import time
+import math
 from geometry import OrientedPoint
 from curve import Curve
 from bisect import bisect_left
@@ -17,10 +19,14 @@ class MovementSupervisor:
         self.linear_speed = linear_speed
         self.angular_speed = angular_speed
         self.linear_curve: Curve
-        self.angular_curve: Curve
+
         self.total_duration: float = 0.0
         self.current_time: float = 0.0
+        self.start_time: float = time.time()
         # self.set_trajectory(pathfinder.get_trajectory())
+
+        self.last_theta = None
+        self.last_time_theta = 0.0
 
     def set_trajectory(self, trajectory):
         """
@@ -38,52 +44,39 @@ class MovementSupervisor:
             self.cumulative_distances.append(total_distance)
         if self.trajectory:
             self._initialize_curves()
+            self.start_time = time.time()
+            self.last_theta = self.trajectory[0].theta
         else:
             raise ValueError("The trajectory cannot be empty.")
 
-    def compute_future_state(self, t):
+    def compute_future_state(self, t=time.time()):
         """
-        Computes the desired point and speeds in t seconds.
+        Computes the desired point and speeds at t seconds.
 
         :param t: Time in seconds for the future state calculation.
         :return: Oriented Point position_desired, linear_speed_desired, angular_speed_desired
         """
-        self.current_time += t
+        self.current_time = t - self.start_time
         if self.current_time > self.total_duration:
             self.current_time = self.total_duration
         future_position: OrientedPoint = self._calculate_future_position(self.current_time)
-        future_linear_speed, future_angular_speed = self._calculate_future_velocity(self.current_time)
+        future_linear_speed, future_angular_speed = self._calculate_future_velocity(self.current_time, future_position.theta)
         return future_position, future_linear_speed, future_angular_speed
 
     def _initialize_curves(self):
         """
-        Initializes the motion curves for linear and angular speeds.
+        Initializes the motion curve for linear speed.
         """
         total_linear_distance = self.cumulative_distances[-1]
-        
-        # Pour simplifier, on suppose que le robot suit une trajectoire plane sans rotation pour le mouvement linéaire
-        # Pour le mouvement angulaire, on calcule la différence totale d'angle
-        if len(self.trajectory) < 2:
-            total_angular_distance = 0
-        else:
-            total_angular_distance = abs(self.trajectory[-1].theta - self.trajectory[0].theta)
 
-        # Profile parameters
         Vd_lin = self.linear_speed
         Vm_lin = self.profile["max_linear_speed"]
-        Va_lin = 0.0 # Assume the robot stops at the end of the movement
+        Va_lin = 0.0 # Assume the robot stops at the end
         amax_lin = self.profile["max_linear_acceleration"]
         dmax_lin = self.profile["max_linear_deceleration"]
         
-        Vd_ang = self.angular_speed
-        Vm_ang = self.profile["max_angular_speed"]
-        Va_ang = 0.0 # Assume the robot stops at the end of the movement
-        amax_ang = self.profile["max_angular_acceleration"]
-        dmax_ang = self.profile["max_angular_deceleration"]
-        
         # Create Curve objects for linear and angular movements
         self.linear_curve = Curve(Vd_lin, Vm_lin, Va_lin, total_linear_distance, amax_lin, dmax_lin)
-        self.angular_curve = Curve(Vd_ang, Vm_ang, Va_ang, total_angular_distance, amax_ang, dmax_ang)
         self.total_duration = self.linear_curve.PlannedTotalTime()
     
     def _calculate_future_position(self, t):
@@ -120,12 +113,29 @@ class MovementSupervisor:
 
         return future_position
 
-    def _calculate_future_velocity(self, t):
+    def _calculate_future_velocity(self, t: float, theta: float):
         """
         Calculates the future speed of the robot at time t.
         """
-        if self.angular_curve is None:
-            raise ValueError("Angular curve is not initialized.")
         v_linear = self.linear_curve.PlannedVelocity(t)
-        v_angular = self.angular_curve.PlannedVelocity(t)
+
+        if self.last_theta is None or self.last_time_theta is None or t == self.last_time_theta:
+            v_angular = self.angular_speed
+        else:
+            dt = t - self.last_time_theta
+            dtheta = theta - self.last_theta
+
+            if dtheta > math.pi:
+                dtheta -= 2 * math.pi
+            elif dtheta < -math.pi:
+                dtheta += 2 * math.pi
+
+            v_angular = dtheta / dt
+
+        if abs(v_angular) > self.profile["max_angular_speed"]:
+            v_angular = self.profile["max_angular_speed"]*(v_angular/abs(v_angular))
+
+        self.last_theta = theta
+        self.last_time_theta = t
+
         return v_linear, v_angular
