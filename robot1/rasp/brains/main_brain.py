07df34c437645a2539e550adaf4e2795afd70e1b
Florian-BARRE <flo.others@gmail.com>
2024-12-12 17:48:58 +0100

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -70,7 +70,7 @@ class MainBrain(Brain):
         finder = PathFinder(
             logger=finder_logger,
             start=OrientedPoint(0, 0, 0.0),
-            goal=OrientedPoint(50, 0, 0.0),
+            goal=OrientedPoint(0, 50, 0.0),
             grid=arena_grid,
             chunk_size=chunk_size
         )
