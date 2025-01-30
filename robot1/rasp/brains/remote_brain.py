4f1c62deb21178053b9e11cb8977f018fe0918cc
Romain-CUCHET <romain.cuchet02@gmail.com>
2025-01-30 17:28:46 +0100

    Refactor PS5 remote handling to use current speed and rotation values

--- robot1/rasp/brains/remote_brain.py
+++ robot1/rasp/brains/remote_brain.py
@@ -78,8 +78,12 @@ class RemoteBrain(Brain):
         get_is_active_brain=lambda self: self.is_active,
     )
     async def handle_ps5_remote(self):
-        speed, angle = self.remote.update_linear_speed()
-        self.rolling_basis.set_speed_and_position(speed, angle, OrientedPoint(0, 0, 0))
+        self.remote.update()
+        self.rolling_basis.set_speed_and_position(
+            self.remote.current_linear_speed,
+            self.remote.current_rotation_speed,
+            OrientedPoint(0, 0),
+        )
 
     @Brain.task(
         process=False,
