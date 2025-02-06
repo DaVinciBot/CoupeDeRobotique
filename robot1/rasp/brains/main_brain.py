4b9191f52e86ee20549230b538eb2d7d5a0e2742
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-02-06 17:51:59 +0100

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -156,6 +156,28 @@ class MainBrain(Brain):
     async def main(self):
         await self.initialize()
 
+        speed_profile: SpeedProfile = SpeedProfile(
+            max_linear_speed=20.0,  # cm/s
+            max_angular_speed=6.0,  # rad/s
+            max_linear_acceleration=3.0,  # cm/s^2
+            max_angular_acceleration=1.0,  # rad/s^2
+            max_linear_deceleration=0.5,  # cm/s^2
+            max_angular_deceleration=1.0,  # rad/s^2
+        )
+        go_to_params = GoToParams(
+            initial_linear_speed=self.rolling_basis.linear_speed,
+            initial_angular_speed=self.rolling_basis.angular_speed,
+            speed_profile=speed_profile,
+            goal=OrientedPoint(250, 140),
+            acs_distance=10,
+            path_finder_recompute_distance=80,
+            timeout=-1.0,
+            is_mandatory=False,
+            smooth_trajectory=True,
+            goal_tolerance=0.1,
+        )
+
+        self.movement_manager.go_to(params=go_to_params)
 
 
 # Only for testing
