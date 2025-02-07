65ef4ecd656c36dad80352cb6ad6dcaabc572c75
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-02-07 13:06:53 +0100

    fix

--- robot1/rasp/config_loader.py
+++ robot1/rasp/config_loader.py
@@ -97,7 +97,7 @@ class CONFIG:
         "kd_linear_speed": 0,
     }
     ANGULAR_SPEED_PID = {
-        "kp_angular_speed": 50,
+        "kp_angular_speed": 80,
         "ki_angular_speed": 0,
         "kd_angular_speed": 0,
     }
--- robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
+++ robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
@@ -113,11 +113,6 @@ void Rolling_Basis::handle(
     double linear_distance_correction = this->linear_distance_pid.compute(Ed);
     double angular_distance_correction = this->angular_distance_pid.compute(Etheta);
 
-    // Je regarde si le cpp déconne niveau calcul ou pas (sinon c python)
-    /*double max_correction = 5.0f;
-    linear_speed_correction = constrain(linear_speed_correction, -max_correction, max_correction);
-    angular_speed_correction = constrain(angular_speed_correction, -max_correction, max_correction);*/
-
     /* Combine both corrections */
     // Compute corrected linear and angular speed
     double Vc = target_linear_speed + linear_speed_correction + linear_distance_correction;
@@ -127,20 +122,11 @@ void Rolling_Basis::handle(
     double right_speed = (2 * Vc + Wc * this->center_distance) / 2;
     double left_speed = (2 * Vc - Wc * this->center_distance) / 2;
 
-    /*double max_motor_speed = 5.0f;  // On limite a la main la vitesse max pour voir encore qui deconne (moi je pense que c'est le calcul)
-    right_speed = constrain(right_speed, -max_motor_speed, max_motor_speed);
-    left_speed = constrain(left_speed, -max_motor_speed, max_motor_speed);*/
     
     
     /* Apply commands to motors */
     this->right_motor->set_motor(right_speed);
     this->left_motor->set_motor(left_speed);
-
-    /// LE PRINT INTERDIT AHHHHH (test puis enlever)
-    /*Serial.print("Vc: "); Serial.print(Vc);
-    Serial.print(" | Wc: "); Serial.print(Wc);
-    Serial.print(" | Right Speed: "); Serial.print(right_speed);
-    Serial.print(" | Left Speed: "); Serial.println(left_speed);*/
 
 }
 
