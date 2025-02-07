b7e888124e55d8b04eeefee85db37eff096681fe
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-02-07 11:43:44 +0100

    fix

--- robot1/teensy_moteur/lib/pid/src/pid.cpp
+++ robot1/teensy_moteur/lib/pid/src/pid.cpp
@@ -24,7 +24,7 @@ float PID::compute(float error)
     // Calculate derivative
     float dedt = (error - this->error_prev) / delta_time;
 
-    if (delta_time < 0.001) delta_time = 0.001;  // Évite les valeurs trop petites de delta time comme ca ca deconne moins du moins jespere
+    //if (delta_time < 0.001) delta_time = 0.001;  // Évite les valeurs trop petites de delta time comme ca ca deconne moins du moins jespere
 
 
     // Calculate integral
--- robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
+++ robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
@@ -114,9 +114,9 @@ void Rolling_Basis::handle(
     double angular_distance_correction = this->angular_distance_pid.compute(Etheta);
 
     // Je regarde si le cpp déconne niveau calcul ou pas (sinon c python)
-    double max_correction = 5.0f;
+    /*double max_correction = 5.0f;
     linear_speed_correction = constrain(linear_speed_correction, -max_correction, max_correction);
-    angular_speed_correction = constrain(angular_speed_correction, -max_correction, max_correction);
+    angular_speed_correction = constrain(angular_speed_correction, -max_correction, max_correction);*/
 
     /* Combine both corrections */
     // Compute corrected linear and angular speed
@@ -127,9 +127,9 @@ void Rolling_Basis::handle(
     double right_speed = (2 * Vc + Wc * this->center_distance) / 2;
     double left_speed = (2 * Vc - Wc * this->center_distance) / 2;
 
-    double max_motor_speed = 5.0f;  // On limite a la main la vitesse max pour voir encore qui deconne (moi je pense que c'est le calcul)
+    /*double max_motor_speed = 5.0f;  // On limite a la main la vitesse max pour voir encore qui deconne (moi je pense que c'est le calcul)
     right_speed = constrain(right_speed, -max_motor_speed, max_motor_speed);
-    left_speed = constrain(left_speed, -max_motor_speed, max_motor_speed);
+    left_speed = constrain(left_speed, -max_motor_speed, max_motor_speed);*/
     
     
     /* Apply commands to motors */
