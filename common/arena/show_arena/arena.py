0d54e5ce5de84d3698394de692beec3a9a30e894
austin <eliottroussille@gmail.com>
2025-02-10 21:37:32 +0100

    goto_positions pour chaque zone. TODO: taille robot+pince, theta et backstage à fixer

--- common/arena/show_arena/arena.py
+++ common/arena/show_arena/arena.py
@@ -74,66 +74,70 @@ class ShowArena(BaseArena):
         )
 
         stuff_zones_points = [
-            ((2.5, 20), (12.5, 60)),
-            ((2.5, 112.5), (12.5, 152.5)),
-            ((57.5, 20), (97.5, 30)),
-            ((62.5, 167.5), (102.5, 177.5)),
-            ((90, 90), (130, 100)),
-            ((300 - 12.5, 20), (300 - 2.5, 60)),
-            ((300 - 12.5, 112.5), (300 - 2.5, 152.5)),
-            ((300 - 97.5, 20), (300 - 57.5, 30)),
-            ((300 - 102.5, 167.5), (300 - 62.5, 177.5)),
-            ((300 - 130, 90), (300 - 90, 100)),
+            ((2.5, 20), (12.5, 60), [OrientedPoint(12,5 + x, 40)]), #TODO: x = distance necessaire entre le robot et les conserves. Theta ??
+            ((2.5, 112.5), (12.5, 152.5), [OrientedPoint(12,5 + x, 132.5)]),
+            ((57.5, 20), (97.5, 30), [OrientedPoint(77.5, 30 + x)]),
+            ((62.5, 167.5), (102.5, 177.5), [OrientedPoint(82.5, 167.5 - x)]),
+            ((90, 90), (130, 100), [OrientedPoint(110, 90 - x), OrientedPoint(110, 100 + x)]),
+            ((300 - 12.5, 20), (300 - 2.5, 60), [OrientedPoint(300 - 12.5 - x, 40)]),
+            ((300 - 12.5, 112.5), (300 - 2.5, 152.5), [OrientedPoint(300 - 12.5 - x, 132.5)]),
+            ((300 - 97.5, 20), (300 - 57.5, 30), [OrientedPoint(300 - 77.5, 30 + x)]),
+            ((300 - 102.5, 167.5), (300 - 62.5, 177.5), [OrientedPoint(300 - 82.5, 167.5 - x)]),
+            ((300 - 130, 90), (300 - 90, 100), [OrientedPoint(300 - 110, 90 - x), OrientedPoint(300 - 110, 100 + x)]),
         ]
 
         yellow_reserved_zones_points = [
-            ((0, 0), (45, 15)),
-            ((0, 65), (45, 110)),
-            ((155,0), (200, 45)),
-            ((200, 0), (245, 15)),
+            ((0, 0), (45, 15), [OrientedPoint(22.5, 15 + x)]), #TODO: x = distance necessaire entre le robot et les zones jaunes. Theta ??
+            ((0, 65), (45, 110), [OrientedPoint(22.5, 65 - x), OrientedPoint(22.5, 110 + x), OrientedPoint(45 + x, 87.5)]),
+            ((155,0), (200, 45), [OrientedPoint(177.5, 45 + x), OrientedPoint(200 + x, 22.5)]),
+            ((200, 0), (245, 15), [OrientedPoint(222.5, 15 + x)]),
         ]
 
         blue_reserved_zones_points = [
-            ((255, 0), (300, 15)),
-            ((255, 65), (300, 110)),
-            ((55, 0), (100, 15)),
-            ((100, 0), (145, 45)),
+            ((255, 0), (300, 15), [OrientedPoint(277.5, 15 + x)]),
+            ((255, 65), (300, 110), [OrientedPoint(277.5, 65 - x), OrientedPoint(277.5, 110 + x), OrientedPoint(255 - x, 87.5)]),
+            ((55, 0), (100, 15), [OrientedPoint(77.5, 15 + x)]),
+            ((100, 0), (145, 45), [OrientedPoint(122.5, 45 + x), OrientedPoint(100 - x, 22.5)]),
         ]
+        #TODO: backstage et pami sans zones ??
 
         forbidden_zones_points = []
 
         zones: list[BaseArenaZone] = []
 
-        for corner_point in stuff_zones_points:
+        for (corner_point1, corner_point2, goto_points) in stuff_zones_points:
             zones.append(
                 StuffZone(
                     logger=stuff_zone_logger,
                     buffer_size=obstacle_buffer,
                     polygon=create_straight_rectangle(
-                        Point(*corner_point[0]), Point(*corner_point[1])
+                        Point(*corner_point1), Point(*corner_point2)
                     ),
+                    goto_positions=goto_points,
                 )
             )
 
-        for corner_point in yellow_reserved_zones_points:
+        for (corner_point1, corner_point2, goto_points) in yellow_reserved_zones_points:
             zones.append(
                 YellowReservedZone(
                     logger=yellow_reserved_zone_logger,
                     buffer_size=obstacle_buffer,
                     polygon=create_straight_rectangle(
-                        Point(*corner_point[0]), Point(*corner_point[1])
+                        Point(*corner_point1), Point(*corner_point2)
                     ),
+                    goto_positions=goto_points,
                 )
             )
 
-        for corner_point in blue_reserved_zones_points:
+        for (corner_point1, corner_point2, goto_points) in blue_reserved_zones_points:
             zones.append(
                 BlueReservedZone(
                     logger=blue_reserved_zone_logger,
                     buffer_size=obstacle_buffer,
                     polygon=create_straight_rectangle(
-                        Point(*corner_point[0]), Point(*corner_point[1])
+                        Point(*corner_point1), Point(*corner_point2)
                     ),
+                    goto_positions=goto_points,
                 )
             )
 
