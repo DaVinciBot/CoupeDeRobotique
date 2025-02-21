c31ca131a458906653772df3f75e78024d289fe6
UrbsKali <urbain.lantres@tutanota.com>
2025-02-21 16:42:21 +0100

    Working OTA update

--- PAMI/src/main.cpp
+++ PAMI/src/main.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h>
+#include "OTA.h"
 
+AsyncWebServer server(80);
+CustomOTA ota("DVB_CDR", "davincibot", &server);
 
 void setup() {
+    Serial.begin(115200);
+    ota.begin();
+    server.begin();
 }
 
 void loop() {
+    ota.loop();
 }
