44608dd7d7b3ff2e8654f8311e12bc5da481ebb2
austin <eliottroussille@gmail.com>
2025-02-26 02:05:24 +0100

    amélioration de l'asservissement

--- PAMI/lib/motors/src/motors.cpp
+++ PAMI/lib/motors/src/motors.cpp
@@ -1,114 +1,105 @@
 #include "motors.h"
 
-Motor::Motor(byte stepPin, byte dirPin, byte enablePin, unsigned int stepsPerRevolution) {
-    _stepPin = stepPin;
-    _dirPin = dirPin;
-    _enablePin = enablePin;
-    _stepsPerRev = stepsPerRevolution;
-    _targetSpeed = 0.0f;
-    _currentSpeed = 0.0f;
+Motor::Motor(byte stepPin, byte dirPin, byte enablePin, unsigned int stepsPerRevolution)
+    : _stepPin(stepPin), _dirPin(dirPin), _enablePin(enablePin), _stepsPerRev(stepsPerRevolution)
+{
+    _targetSpeedStepsPerSec = 0.0f;
+    _currentSpeedStepsPerSec = 0.0f;
     _acceleration = 0.0f;
     _moving = false;
-    _stepsRemaining = 0;
     _lastStepTime = 0;
     _stepIntervalUs = 0.0f;
 }
 
-void Motor::init() {
+void Motor::init()
+{
     pinMode(_stepPin, OUTPUT);
     pinMode(_dirPin, OUTPUT);
     pinMode(_enablePin, OUTPUT);
     enableMotor(false);
 }
 
-void Motor::enableMotor(bool enable) {
+void Motor::enableMotor(bool enable)
+{
     digitalWrite(_enablePin, enable ? LOW : HIGH);
     // digitalWrite(_enablePin, enable ? HIGH : LOW); demander à l'elec le branchement
 }
 
-void Motor::setTargetSpeed(float stepsPerSec) {
-    if(stepsPerSec < 0) stepsPerSec = 0;
-    _targetSpeed = stepsPerSec;
+void Motor::setTargetSpeed(float stepsPerSec)
+{
+    if (stepsPerSec < 0)
+        stepsPerSec = 0;
+    _targetSpeedStepsPerSec = stepsPerSec;
+    _moving = (_targetSpeedStepsPerSec > 0);
 }
 
-void Motor::setAcceleration(float accel) {
-    if(accel < 0) accel = 0;
-    _acceleration = accel;
+void Motor::setAcceleration(float stepsPerSec2)
+{
+    if (stepsPerSec2 < 0)
+        stepsPerSec2 = 0;
+    _acceleration = stepsPerSec2;
 }
 
-void Motor::moveSteps(long steps) {
-    bool clockwise = (steps >= 0);
-    long nbSteps = labs(steps);
-
-    _setDirection(clockwise);
-
-    _stepsRemaining = nbSteps;
-
-    _moving = true;
-
-    _lastStepTime = micros();
-    if(_currentSpeed < 1.0f) {
-        _stepIntervalUs = 1000000.0f;
-    } else {
-        _stepIntervalUs = 1000000.0f / _currentSpeed;
-    }
-}
-
-void Motor::moveDistance(float distanceMm, float wheelDiameterMm) {
-    float circumference = M_PI * wheelDiameterMm;
-    float turns = distanceMm / circumference;
-    long steps = (long)(turns * _stepsPerRev);
-    moveSteps(steps);
-}
-
-void Motor::update() {
-    if(!_moving || _stepsRemaining <= 0) {
-        _moving = false;
-        return;
-    }
-
-    unsigned long now = micros();
-    unsigned long dt = now - _lastStepTime;
-
-    float dtSec = (float)dt / 1000000.0f;
-    float speedDiff = _acceleration * dtSec;
-
-    if(_currentSpeed < _targetSpeed) {
-        _currentSpeed += speedDiff;
-        if(_currentSpeed > _targetSpeed) {
-            _currentSpeed = _targetSpeed;
-        }
-    } else if(_currentSpeed > _targetSpeed) {
-        _currentSpeed -= speedDiff;
-        if(_currentSpeed < _targetSpeed) {
-            _currentSpeed = _targetSpeed;
-        }
-    }
-
-    if(_currentSpeed < 1.0f) {
-        _stepIntervalUs = 1000000.0f; // 1 s
-    } else {
-        _stepIntervalUs = 1000000.0f / _currentSpeed;
-    }
-
-    if(dt >= _stepIntervalUs) {
-        _doOneStep();
-
-        _lastStepTime = micros();
-        _stepsRemaining--;
-
-        if(_stepsRemaining <= 0) {
-            _moving = false;
-        }
-    }
-}
-
-void Motor::_setDirection(bool clockwise) {
+void Motor::_setDirection(bool clockwise)
+{
     digitalWrite(_dirPin, clockwise ? HIGH : LOW);
 }
 
-void Motor::_doOneStep() {
+void Motor::_doOneStep()
+{
     digitalWrite(_stepPin, HIGH);
-    delayMicroseconds(2); // ?????
+    delayMicroseconds(2);
     digitalWrite(_stepPin, LOW);
+}
+
+void Motor::update()
+{
+    if (!_moving)
+        return;
+
+    unsigned long now = micros();
+    unsigned long dt = now - _lastStepTime;
+    float dtSec = dt / 1000000.0f;
+    float speedDiff = _acceleration * dtSec;
+
+    if (_currentSpeedStepsPerSec < _targetSpeedStepsPerSec)
+    {
+        _currentSpeedStepsPerSec += speedDiff;
+        if (_currentSpeedStepsPerSec > _targetSpeedStepsPerSec)
+        {
+            _currentSpeedStepsPerSec = _targetSpeedStepsPerSec;
+        }
+    }
+    else if (_currentSpeedStepsPerSec > _targetSpeedStepsPerSec)
+    {
+        _currentSpeedStepsPerSec -= speedDiff;
+        if (_currentSpeedStepsPerSec < _targetSpeedStepsPerSec)
+        {
+            _currentSpeedStepsPerSec = _targetSpeedStepsPerSec;
+        }
+    }
+
+    if (_currentSpeedStepsPerSec < 1.0f)
+    {
+        _stepIntervalUs = 1e6;
+    }
+    else
+    {
+        _stepIntervalUs = 1e6 / _currentSpeedStepsPerSec;
+    }
+
+    bool clockwise = (_currentSpeedStepsPerSec >= 0);
+    _setDirection(clockwise);
+
+    if (dt >= _stepIntervalUs)
+    {
+        _doOneStep();
+        _lastStepTime = micros();
+    }
+
+    // Stoppage si target = 0
+    if (_targetSpeedStepsPerSec < 1.0f && _currentSpeedStepsPerSec < 1.0f)
+    {
+        _moving = false;
+    }
 }
--- PAMI/src/main.cpp
+++ PAMI/src/main.cpp
@@ -3,58 +3,38 @@
 #include "navigation.h"
 
 #define LEFT_STEP_PIN 2
-#define LEFT_DIR_PIN  3
-#define LEFT_EN_PIN   4
+#define LEFT_DIR_PIN 3
+#define LEFT_EN_PIN 4
 
 #define RIGHT_STEP_PIN 5
-#define RIGHT_DIR_PIN  6
-#define RIGHT_EN_PIN   7
-
+#define RIGHT_DIR_PIN 6
+#define RIGHT_EN_PIN 7
 
 Motor leftMotor(LEFT_STEP_PIN, LEFT_DIR_PIN, LEFT_EN_PIN, 200);
 Motor rightMotor(RIGHT_STEP_PIN, RIGHT_DIR_PIN, RIGHT_EN_PIN, 200);
 
-Navigation nav(&leftMotor, &rightMotor, 65, 150);
+Navigation nav(&leftMotor, &rightMotor, 65.0f, 150.0f);
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     leftMotor.init();
     rightMotor.init();
-
     leftMotor.enableMotor(true);
-    leftMotor.setTargetSpeed(300);
-    leftMotor.setAcceleration(100);
-
     rightMotor.enableMotor(true);
-    rightMotor.setTargetSpeed(300);
+
+    leftMotor.setAcceleration(100);
     rightMotor.setAcceleration(100);
 
-    Serial.println("Fin init");
+    Serial.println("Démarrage du robot");
 }
 
-void loop() {
-    if(!nav.isBusy()) {
-        static int state = 0;
-        switch(state) {
-            case 0:
-                Serial.println("Avance de 200 mm");
-                nav.forward(200);
-                state = 1;
-                break;
-            case 1:
-                Serial.println("Tourne 90 deg à gauche");
-                nav.turn(90);
-                state = 2;
-                break;
-            case 2:
-                Serial.println("Avance de 100 mm");
-                nav.forward(100);
-                state = 3;
-                break;
-            default:
-                break;
-        }
+void loop()
+{
+    if (!nav.isBusy())
+    {
+        nav.setLinearAngularSpeed(50.0f, 45.0f);
     }
 
     nav.update();
