de402c2d9d1fe1adf92d12bdec673ab32b89fd12
Florian-BARRE <flo.others@gmail.com>
2025-03-05 13:48:23 +0100

    update wscomms logging

--- robot1/rasp/main.py
+++ robot1/rasp/main.py
@@ -53,6 +53,18 @@ if __name__ == "__main__":
         identifier="WS_Server",
         follow_logger_manager_rules=True,
     )
+    logger_ws_cmd_route_manager = Logger(
+        identifier="WS_cmd_RouteManager",
+        follow_logger_manager_rules=True,
+    )
+    logger_ws_cmd_sender = Logger(
+        identifier="WS_cmd_Sender",
+        follow_logger_manager_rules=True,
+    )
+    logger_ws_cmd_receiver = Logger(
+        identifier="WS_cmd_Receiver",
+        follow_logger_manager_rules=True,
+    )
     logger_brain = Logger(
         identifier="Brain",
         # Only Brain manages monitoring
@@ -82,15 +94,18 @@ if __name__ == "__main__":
 
     """ Main object instances """
     # Websocket server
+    # Websocket server
     ws_server = WServer(
         logger=logger_ws_server,
         host=CONFIG.WS_HOSTNAME,
         port=CONFIG.WS_PORT,
-        ping_pong_clients_interval=CONFIG.WS_PING_PONG_INTERVAL,  # TODO: To fix, this feature is not working
+        # ping_pong_clients_interval=CONFIG.WS_PING_PONG_INTERVAL,  # TODO: To fix, this feature is not working
     )
     # Routes
     ws_cmd = WServerRouteManager(
-        WSreceiver(use_queue=True), WSender(CONFIG.WS_SENDER_NAME)
+        logger=logger_ws_cmd_route_manager,
+        receiver=WSreceiver(logger=logger_ws_cmd_receiver, use_queue=True),
+        sender=WSender(logger=logger_ws_cmd_sender, name=CONFIG.WS_SENDER_NAME)
     )
     ws_server.add_route_handler(CONFIG.WS_CMD_ROUTE, ws_cmd)
 
