e30c0eb5d19f6cb87735babe54649f565f4f1752
Florian-BARRE <flo.others@gmail.com>
2025-03-12 19:11:02 +0100

    return [] when no path found

--- common/path_finding/path_finder.py
+++ common/path_finding/path_finder.py
@@ -363,6 +363,10 @@ class PathFinder:
         """
         self.__find_path(use_static_and_dynamic_grid=use_static_and_dynamic_grid)
 
+        # If no path found, return an empty list
+        if not self.path_found:
+            return []
+        
         # Add real robot position as start point and goal as end point (not approximated chunk points)
         if not smooth_path:
             self.oriented_path_found = (
