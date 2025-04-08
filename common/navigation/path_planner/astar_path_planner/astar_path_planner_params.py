86c378d9f53e152d64cbf70404ee46a2db0fd959
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-04-08 15:09:38 +0200

    astar_planner implementation

--- common/navigation/path_planner/astar_path_planner/astar_path_planner_params.py
+++ common/navigation/path_planner/astar_path_planner/astar_path_planner_params.py
@@ -4,8 +4,12 @@
 from navigation.path_planner.structs import PathPlanningStrategy, Direction
 from navigation.path_planner.base_path_planner.base_path_planner_params import BasePathPlannerParams, \
     BasePathPlannerPlanPathParams
-from path_finding import PathFinder
+
+# Third-party imports
 from pathfinding.core.grid import Grid
+from pathfinding.core.grid import GridNode
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.finder.a_star import AStarFinder
 
 from geometry import OrientedPoint
 
@@ -16,21 +20,59 @@ class AStarPathPlannerParams(BasePathPlannerParams):
 
     Attributes:
         direction (Direction): Indicates whether the path should be planned FORWARD or BACKWARD.
-        current_grid_state (Grid): Current state of the grid for pathfinding.
+        grid (Grid): Current grid for pathfinding.
+        path_resolution (float): Resolution for path smoothing.
+        absolute_current_position (OrientedPoint): Absolute position of the robot in the grid.
+        absolute_goal (OrientedPoint): Absolute goal position in the grid.
+        current_position (GridNode): Current position of the robot in grid coordinates.
+        goal (GridNode): Goal position in grid coordinates.
+        finder (AStarFinder): Instance of the A* pathfinding algorithm.
+        path_found (list[GridNode]): List of grid nodes representing the found path.
+        oriented_path_found (list[OrientedPoint]): List of oriented points representing the path with orientation.
     """
 
-    def __init__(self, direction: Direction = Direction.FORWARD, path_finder: PathFinder = None,
-                 current_grid_state: Grid = None):
+    def __init__(self,
+                 grid: Grid,
+                 path_resolution: float,
+                 chunk_size: int,
+                 start: OrientedPoint,
+                 goal: OrientedPoint,
+                 direction: Direction = Direction.FORWARD,
+                 ):
         """
         Initialize parameters for the A* path planner.
 
         Args:
-            direction (Direction): Direction of motion (default is FORWARD).
-            path_finder (PathFinder): Instance of the path finder (default is None).
-            current_grid_state (Grid): Current state of the grid (default is None).
+            direction (Direction): Indicates whether the path should be planned FORWARD or BACKWARD.
+            grid (Grid): Current the grid for pathfinding.
+            path_resolution (float): Resolution for path smoothing.
         """
+        # Direction parameter
         self.direction: Direction = direction
-        self.current_grid_state: Grid = current_grid_state
+
+        # Grid related parameters
+        self.grid: Grid = grid
+
+        self.chunk_size: int = chunk_size
+        self.half_chunk_size: float = self.chunk_size / 2
+
+        self.grid_width: int = grid.width
+        self.grid_height: int = grid.height
+
+        # Pathfinding related parameters
+        self.path_resolution: float = path_resolution
+
+        self.absolute_current_position: OrientedPoint = start
+        self.absolute_goal: OrientedPoint = goal
+
+        self.current_position: GridNode = GridNode(0, 0)
+        self.goal: GridNode = GridNode(0, 0)
+
+        self.finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+
+        self.path_found: list[GridNode] = []
+        self.oriented_path_found: list[OrientedPoint] = []
+
         super().__init__(PathPlanningStrategy.A_STAR)
 
 
