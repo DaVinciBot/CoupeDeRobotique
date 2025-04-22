7cae8ecc884129ef59d6bda0f467ef49c8b1b184
austin <eliottroussille@gmail.com>
2025-04-22 18:22:37 +0200

    🚑 fix linear ramped profile

--- common/navigation/trajectory_planner/speed_profile/linear_ramped_speed_profile.py
+++ common/navigation/trajectory_planner/speed_profile/linear_ramped_speed_profile.py
@@ -36,15 +36,6 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
         self.acceleration = acceleration
         self.deceleration = deceleration
 
-    def _decel_rate(self):
-        """
-        Computes the deceleration rate based on the max speed and configured deceleration time.
-
-        Returns:
-            float: Deceleration rate.
-        """
-        return self._max_speed / self.deceleration
-
     def _compute_trapezoidal_params(self, departure_speed, arrival_speed):
         """
         Computes parameters used in trapezoidal motion profile.
@@ -54,19 +45,16 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
             arrival_speed (float): Speed at the end of the motion.
 
         Returns:
-            tuple: Time to accelerate, time to decelerate, distance during acceleration,
-                   distance during deceleration, and deceleration rate.
+            tuple: Time to accelerate, time to decelerate, distance during acceleration
+            and distance during deceleration.
         """
-        decel = self._decel_rate()
         t_acc = (self._max_speed - departure_speed) / self.acceleration
-        t_dec = (self._max_speed - arrival_speed) / decel
+        t_dec = (self._max_speed - arrival_speed) / self.deceleration
         d_acc = (self._max_speed**2 - departure_speed**2) / (2 * self.acceleration)
-        d_dec = (self._max_speed**2 - arrival_speed**2) / (2 * decel)
-        return t_acc, t_dec, d_acc, d_dec, decel
+        d_dec = (self._max_speed**2 - arrival_speed**2) / (2 * self.deceleration)
+        return t_acc, t_dec, d_acc, d_dec
 
-    def _compute_triangular_peak_speed(
-        self, distance, departure_speed, arrival_speed, decel_rate
-    ):
+    def _compute_triangular_peak_speed(self, distance, departure_speed, arrival_speed):
         """
         Calculates peak speed for triangular profile when cruising is not possible.
 
@@ -79,10 +67,10 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
         Returns:
             float: Computed peak speed.
         """
-        v_peak_denom = (1 / (2 * self.acceleration)) + (1 / (2 * decel_rate))
+        v_peak_denom = (1 / (2 * self.acceleration)) + (1 / (2 * self.deceleration))
         v_peak_sq = (
             (departure_speed**2 / (2 * self.acceleration))
-            + (arrival_speed**2 / (2 * decel_rate))
+            + (arrival_speed**2 / (2 * self.deceleration))
             + distance
         ) / v_peak_denom
         return sqrt(v_peak_sq)
@@ -105,7 +93,7 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
         if time_elapsed is None or time_elapsed < 0:
             return departure_speed
 
-        t_acc, t_dec, d_acc, d_dec, decel_rate = self._compute_trapezoidal_params(
+        t_acc, t_dec, d_acc, d_dec = self._compute_trapezoidal_params(
             departure_speed, arrival_speed
         )
 
@@ -115,8 +103,10 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
                 return departure_speed + self.acceleration * time_elapsed
             elif time_elapsed < t_acc + t_dec:
                 return max(
-                    self._max_speed - decel_rate * (time_elapsed - t_acc), arrival_speed
+                    arrival_speed + self.deceleration * (t_acc + t_dec - time_elapsed),
+                    arrival_speed,
                 )
+
             else:
                 return arrival_speed
 
@@ -132,7 +122,8 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
                 return self._max_speed
             elif time_elapsed < t_total:
                 return max(
-                    self._max_speed - decel_rate * (time_elapsed - t_acc - t_cruise),
+                    self._max_speed
+                    - self.deceleration * (time_elapsed - t_acc - t_cruise),
                     arrival_speed,
                 )
             else:
@@ -140,16 +131,18 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
         else:
             # Triangular profile (no cruising)
             v_peak = self._compute_triangular_peak_speed(
-                distance, departure_speed, arrival_speed, decel_rate
+                distance, departure_speed, arrival_speed
             )
             t_peak = (v_peak - departure_speed) / self.acceleration
-            t_decel = (v_peak - arrival_speed) / decel_rate
+            t_decel = (v_peak - arrival_speed) / self.deceleration
             t_total = t_peak + t_decel
 
             if time_elapsed < t_peak:
                 return departure_speed + self.acceleration * time_elapsed
             elif time_elapsed < t_total:
-                return max(v_peak - decel_rate * (time_elapsed - t_peak), arrival_speed)
+                return max(
+                    v_peak - self.deceleration * (time_elapsed - t_peak), arrival_speed
+                )
             else:
                 return arrival_speed
 
@@ -171,7 +164,7 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
         if time_elapsed <= 0:
             return 0.0
 
-        t_acc, t_dec, d_acc, d_dec, decel_rate = self._compute_trapezoidal_params(
+        t_acc, t_dec, d_acc, d_dec = self._compute_trapezoidal_params(
             departure_speed, arrival_speed
         )
 
@@ -185,7 +178,12 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
                 )
             elif time_elapsed < t_total:
                 dt = time_elapsed - t_acc
-                return d_acc + self._max_speed * dt - 0.5 * decel_rate * dt**2
+                return (
+                    d_acc
+                    + arrival_speed * dt
+                    + self.deceleration
+                    * (t_total * dt - ((time_elapsed**2 - t_acc**2) / 2))
+                )
             else:
                 return d_acc + d_dec
 
@@ -205,17 +203,21 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
             elif time_elapsed < t_total:
                 dt = time_elapsed - (t_acc + t_cruise)
                 return (
-                    d_acc + d_cruise + self._max_speed * dt - 0.5 * decel_rate * dt**2
+                    d_acc
+                    + d_cruise
+                    + arrival_speed * dt
+                    + self.deceleration
+                    * (t_total * dt - ((time_elapsed**2 - (t_acc + t_cruise) ** 2) / 2))
                 )
             else:
                 return distance
         else:
             # Triangular profile
             v_peak = self._compute_triangular_peak_speed(
-                distance, departure_speed, arrival_speed, decel_rate
+                distance, departure_speed, arrival_speed
             )
             t_peak = (v_peak - departure_speed) / self.acceleration
-            t_decel = (v_peak - arrival_speed) / decel_rate
+            t_decel = (v_peak - arrival_speed) / self.deceleration
             t_total = t_peak + t_decel
 
             if time_elapsed < t_peak:
@@ -225,10 +227,13 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
                 )
             elif time_elapsed < t_total:
                 dt = time_elapsed - t_peak
-                d_acc_peak = (
-                    departure_speed * t_peak + 0.5 * self.acceleration * t_peak**2
+                d_acc_peak = (v_peak**2 - departure_speed**2) / (2 * self.acceleration)
+                return (
+                    d_acc_peak
+                    + arrival_speed * dt
+                    + self.deceleration
+                    * (t_total * dt - ((time_elapsed**2 - t_peak**2) / 2))
                 )
-                return d_acc_peak + v_peak * dt - 0.5 * decel_rate * dt**2
             else:
                 return distance
 
@@ -246,7 +251,7 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
         Returns:
             float: Total duration of the motion profile.
         """
-        t_acc, t_dec, d_acc, d_dec, decel_rate = self._compute_trapezoidal_params(
+        t_acc, t_dec, d_acc, d_dec = self._compute_trapezoidal_params(
             departure_speed, arrival_speed
         )
 
@@ -256,8 +261,8 @@ class LinearRampedSpeedProfile(BaseSpeedProfile):
             return t_acc + t_cruise + t_dec
         else:
             v_peak = self._compute_triangular_peak_speed(
-                distance, departure_speed, arrival_speed, decel_rate
+                distance, departure_speed, arrival_speed
             )
             t_peak = (v_peak - departure_speed) / self.acceleration
-            t_decel = (v_peak - arrival_speed) / decel_rate
+            t_decel = (v_peak - arrival_speed) / self.deceleration
             return t_peak + t_decel
