4a7214de301a78f229dd4a04c0dea5b2d3dcf43b
UrbsKali <urbain.lantres@tutanota.com>
2025-04-24 23:44:05 +0200

    WIP PAMI (runtime error)

--- PAMI/include/config.h
+++ PAMI/include/config.h
@@ -1,10 +1,26 @@
 #include <Arduino.h>
-#include <OTA.h>
+#include "motor.h"
+#include "navigation.h"
+#include "rolling_basis.h"
 
+
+// #------- MOTOR CONFIGURATION -------#
+#define LEFT_STEP_PIN 16
+#define LEFT_DIR_PIN 15
+#define LEFT_EN_PIN 17
+
+#define RIGHT_STEP_PIN 24
+#define RIGHT_DIR_PIN 23
+#define RIGHT_EN_PIN 25
+
+// #------- LORA CONFIGURATION -------#
 #define SS 10 // NSS pin
 #define RST 9 // RESET pin
 #define BUSY 8 // BUSY pin
 #define IRQ 7 // IRQ pin
 #define TXEN 6 // TXEN pin
 #define RXEN 5 // RXEN pin
+
+// #-------- DEV CONFIGURATION ---------#
+#define ENABLE_OTA false
 
--- PAMI/src/main.cpp
+++ PAMI/src/main.cpp
@@ -1,35 +1,17 @@
-#include <Arduino.h>
-#include "OTA.h"
-#include "motor.h"
-#include "navigation.h"
-#include "rolling_basis.h"
-
-#define LEFT_STEP_PIN 2
-#define LEFT_DIR_PIN 3
-#define LEFT_EN_PIN 1
-
-Motor leftMotor(LEFT_STEP_PIN, LEFT_DIR_PIN, LEFT_EN_PIN, 400);
 // #include "com.h"
 #include "config.h"
 
+Motor leftMotor(LEFT_STEP_PIN, LEFT_DIR_PIN, LEFT_EN_PIN, 400);
+Motor rightMotor(RIGHT_STEP_PIN, RIGHT_DIR_PIN, RIGHT_EN_PIN, 400);
+
+#if ENABLE_OTA
+#include "OTA.h"
 AsyncWebServer server(80);
 CustomOTA ota("DVB", "davincibot", &server);
+#endif
+
 // Com *com = new Com(); // LoRa object
-int counter = 0;
 
-void setup()
-{
-    Serial.begin(115200);
-
-    leftMotor.init();
-
-    leftMotor.enableMotor(true);
-
-    leftMotor.setAcceleration(100);
-
-    leftMotor.setTargetSpeed(400);
-
-    digitalWrite(LEFT_DIR_PIN, HIGH);
 void print_debug(byte *msg, byte size)
 {
     Serial.print("Received message: ");
@@ -54,67 +36,34 @@ void initialize_callback_functions()
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("\n-- LORA TRANSMITTER / RECEIVER --\n");
+    Serial.println("\n-- PAMI test --\n");
     // com->begin(SS, RST, BUSY, IRQ, TXEN, RXEN); // NSS, RESET, BUSY, IRQ, TXEN, RXEN pins
+    leftMotor.init();
+    leftMotor.enableMotor(true);
+    leftMotor.setAcceleration(100);
+    leftMotor.setTargetSpeed(400);
 
+    rightMotor.init();
+    rightMotor.enableMotor(true);
+    rightMotor.setAcceleration(100);
+    rightMotor.setTargetSpeed(400);
+
+    digitalWrite(LEFT_DIR_PIN, HIGH);
+    digitalWrite(RIGHT_DIR_PIN, HIGH);
+
+#if ENABLE_OTA
     ota.begin();
+#endif
 
     initialize_callback_functions();
 }
 
 void loop()
 {
+    leftMotor.update();
+    rightMotor.update();
 
-    for (int i = 0; i < 800; i++)
-    {
-        leftMotor.update();
-    }
-
+#if ENABLE_OTA
     ota.loop();
-}
-    // com->handle_callback(callback_functions);
-    ota.loop(); // Handle OTA updates
-
-    // if (counter++ > 4096)
-    // {
-    //     // com->print("test message"); // Send a test message
-    //     counter = 0;
-    // }
-
-    // ---- RECEIVE MODE ----
-    // Request for receiving new LoRa packet
-    //   LoRa.request();
-    //   // Wait for incoming LoRa packet
-    //   LoRa.wait();
-
-    //   // Read received message and counter
-    //   const uint8_t msgLen = LoRa.available() - 1;
-    //   char receivedMessage[msgLen];
-    //   uint8_t receivedCounter;
-
-    //   uint8_t i = 0;
-    //   while (LoRa.available() > 1) {
-    //     receivedMessage[i++] = LoRa.read();
-    //   }
-    //   receivedCounter = LoRa.read();
-
-    //   // Print received message and counter
-    //   Serial.print("Received: ");
-    //   Serial.print(receivedMessage);
-    //   Serial.print("  ");
-    //   Serial.println(receivedCounter);
-
-    //   // Print packet/signal status including RSSI and SNR
-    //   Serial.print("Packet status: RSSI = ");
-    //   Serial.print(LoRa.packetRssi());
-    //   Serial.print(" dBm | SNR = ");
-    //   Serial.print(LoRa.snr());
-    //   Serial.println(" dB");
-
-    //   // Show received status in case of CRC or header error
-    //   uint8_t status = LoRa.status();
-    //   if (status == SX126X_STATUS_CRC_ERR) {
-    //     Serial.println("CRC error");
-    //   } else if (status == SX126X_STATUS_HEADER_ERR) {
-    //     Serial.println("Packet header error");
+#endif
 }
