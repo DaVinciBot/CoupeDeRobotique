d4fc32be80e1aa8082a23a8d87c394ec15f96ea2
Yliux <148073646+Yliux@users.noreply.github.com>
2025-04-24 19:11:29 +0200

    Update main.cpp

--- PAMI/src/main.cpp
+++ PAMI/src/main.cpp
@@ -3,10 +3,19 @@
 #include "motor.h"
 #include "navigation.h"
 #include "rolling_basis.h"
+#include "gs2_lidar.hpp"
 
 #define LEFT_STEP_PIN 2
 #define LEFT_DIR_PIN 3
 #define LEFT_EN_PIN 1
+
+#define RX_LIDAR 44
+#define TX_LIDAR 43
+
+Gs2Lidar lidar(Serial1, RX_LIDAR, TX_LIDAR,
+               116,   // à modif
+               130);  // à modif si necessaire
+
 
 Motor leftMotor(LEFT_STEP_PIN, LEFT_DIR_PIN, LEFT_EN_PIN, 400);
 
@@ -16,6 +25,8 @@ CustomOTA ota("DVB_CDR", "davincibot", &server);
 void setup()
 {
     Serial.begin(115200);
+    
+    lidar.begin();
 
     leftMotor.init();
 
@@ -33,11 +44,12 @@ void setup()
 
 void loop()
 {
+    lidar.task();
 
-    for (int i = 0; i < 800; i++)
-    {
-        leftMotor.update();
-    }
+     if (lidar.obstacleDetected())
+        Serial.println("Obstacle proche !");
+    if (lidar.cliffDetected())
+        Serial.println("Vide détecté !");
 
     ota.loop();
 }
