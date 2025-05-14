639852a8b60e31ea74f6ff02bf9eba03a2698e07
gaelAphack <gael.le_mouel@edu.devinci.fr>
2025-05-14 14:36:00 +0200

    Change debug message

--- robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
+++ robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
@@ -132,7 +132,8 @@ void Rolling_Basis::handle(
 
     if (orientation_error > PI / 2 || orientation_error < -PI / 2)
     {
-        distance_error = -distance_error;
+        com->print("Through backward")
+            distance_error = -distance_error;
         orientation_error = fmod(PI + orientation_error, PI);
     }
 
