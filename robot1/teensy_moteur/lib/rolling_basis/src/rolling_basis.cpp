f3a81c7e1bb3245b26560ce5036fcc0ecead0150
austin <eliottroussille@gmail.com>
2025-05-14 20:18:34 +0200

    fix distance error

--- robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
+++ robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
@@ -135,6 +135,9 @@ void Rolling_Basis::handle(
     double xerr = target_position.x - this->X;
     double yerr = target_position.y - this->Y;
     double distance_error = sqrt(pow(xerr, 2) + pow(yerr, 2));
+    double forward_err = xerr * cos(this->THETA) + yerr * sin(this->THETA);
+    double distance_error *= (forward_err > 0.0) ? 1 : -1;
+
     double theta_error_odometry = target_position.theta - this->THETA; // fmod to keep the angle between -PI and PI, TODO: a tester !!
     // double orientation_error = fmod(atan2(yerr, xerr) + theta_error_odometry - this->THETA, 2 * PI);
     double orientation_error = theta_error_odometry;
