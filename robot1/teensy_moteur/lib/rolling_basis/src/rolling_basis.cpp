dd1dee2431e6dede906b5b80425d431b11fd3eb1
austin <eliottroussille@gmail.com>
2025-05-14 20:32:26 +0200

    fix distance signée error

--- robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
+++ robot1/teensy_moteur/lib/rolling_basis/src/rolling_basis.cpp
@@ -134,9 +134,9 @@ void Rolling_Basis::handle(
     // Compute distance and orientation error (difference between target and real)
     double xerr = target_position.x - this->X;
     double yerr = target_position.y - this->Y;
-    double distance_error = sqrt(pow(xerr, 2) + pow(yerr, 2));
+    // double distance_error = sqrt(pow(xerr, 2) + pow(yerr, 2));
     double forward_err = xerr * cos(this->THETA) + yerr * sin(this->THETA);
-    distance_error *= (forward_err > 0.0) ? 1 : -1;
+    double distance_error = (forward_err > 0.0) ? forward_err : -forward_err;
 
     double theta_error_odometry = target_position.theta - this->THETA; // fmod to keep the angle between -PI and PI, TODO: a tester !!
     // double orientation_error = fmod(atan2(yerr, xerr) + theta_error_odometry - this->THETA, 2 * PI);
