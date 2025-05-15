4ec3ff954c9be26d73d0ee59b43c486e3e41ba2c
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-15 12:15:42 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -109,8 +109,10 @@ class MainBrain(Brain):
         )
         rolling_basis.set_odometrie(self.rolling_basis_odometrie)
         
+        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(50, 25, 0)) # Point d'arrivée
+        await asyncio.sleep(0.1)
         rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(20, 25, 90)) # Point d'arrivée
-        
+
         #rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(50, 25, -90))
         # --- MetaProg is insane (loop) --- #
         if self.navigator_task is not None:
