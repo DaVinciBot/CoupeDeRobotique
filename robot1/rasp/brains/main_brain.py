c39a9904f215ae613dd1cb2b64ae171fd25309d8
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-15 14:45:46 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -116,7 +116,7 @@ class MainBrain(Brain):
 
         rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(40, 25, 0))
         time.sleep(3)
-        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(40, 25, -90)) # Point d'arrivée
+        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(40, 25, 90)) # Point d'arrivée
         time.sleep(3)
         #rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(60, 25, 0)) # Point d'arrivée
         # time.sleep(3)
