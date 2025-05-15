f8454148f376dc126d52409c5879819a8c148535
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-15 15:11:19 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -115,7 +115,9 @@ class MainBrain(Brain):
         #time.sleep(500) A utiliser pour les matchs pour l'instant
 
 
-        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(20, 25, -90))
+        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(20, 25, 45))
+        time.sleep(0.5)
+        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(20, 25, 45))
     
 
         #rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(50, 25, -90))
