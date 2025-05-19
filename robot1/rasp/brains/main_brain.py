ef9d9ce95f4464d68ee991490506e187189d1161
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-19 18:52:38 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -114,7 +114,7 @@ class MainBrain(Brain):
         )
         rolling_basis.set_odometrie(self.rolling_basis_odometrie)
 
-        self.wait_for_trigger()
+        #self.wait_for_trigger()
         # time.sleep(500) A utiliser pour les matchs pour l'instant
 
         for i in range(20, 51, 0.5):
