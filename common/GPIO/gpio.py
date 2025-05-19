66adaaf943cc70b221eb57dbb23dd0a2308bd3c4
austin <eliottroussille@gmail.com>
2025-05-19 18:03:38 +0200

    fix gpio

--- common/GPIO/gpio.py
+++ common/GPIO/gpio.py
@@ -1,5 +1,6 @@
 # import RPi.GPIO as GPIO
 from gpiozero import LED, InputDevice
+from gpiozero.pins.lgpio import LGPIOFactory
 
 
 class PIN:
@@ -36,14 +37,18 @@ class PIN:
         self.reverse_state = reverse_state
 
         if mode == "output":
-            self.device = LED(self.pin)
+            self.device = LED(self.pin, pin_factory=LGPIOFactory())
             self.device.off()
         elif mode == "input":
-            self.device = InputDevice(self.pin)
+            self.device = InputDevice(self.pin, pin_factory=LGPIOFactory())
         elif mode == "input_pullup":
-            self.device = InputDevice(self.pin, active_state=False, pull_up=True)
+            self.device = InputDevice(
+                self.pin, active_state=False, pull_up=True, pin_factory=LGPIOFactory()
+            )
         elif mode == "input_pulldown":
-            self.device = InputDevice(self.pin, active_state=False, pull_up=False)
+            self.device = InputDevice(
+                self.pin, active_state=False, pull_up=False, pin_factory=LGPIOFactory()
+            )
 
     def digital_write(self, state: bool):
         """
