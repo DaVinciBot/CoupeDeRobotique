30594cbb323aa94d9aaa3e8942b9f3320eafca14
Florian-BARRE <flo.others@gmail.com>
2025-05-19 20:25:52 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -99,13 +99,13 @@ class MainBrain(Brain):
         )
         rolling_basis.set_odometrie(self.rolling_basis_odometrie)
         
-        for i in range(20, 51):
+        for i in range(20, 51, 0.1):
             rolling_basis.set_speed_and_position(
                 target_linear_speed=0,
                 target_angular_speed=0,
                 target_position=OrientedPoint(i, 25, 0),
             )
-            time.sleep(0.1)
+            time.sleep(0.01)
         
 
         # --- MetaProg is insane (loop) --- #
