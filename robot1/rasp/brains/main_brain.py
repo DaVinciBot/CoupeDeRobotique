ae03a99194fe9987a8a6b3aeb3813c01a53b702b
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-19 10:41:04 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -114,7 +114,7 @@ class MainBrain(Brain):
         #self.wait_for_trigger() A faire après avoir fix GPIO
         #time.sleep(500) A utiliser pour les matchs pour l'instant
         
-        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(70, 25, 0))
+        rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(50, 25, 0))
         time.sleep(4)
         rolling_basis.set_speed_and_position(0.0, 0.0, OrientedPoint(50, 25, -90))
         time.sleep(4)
