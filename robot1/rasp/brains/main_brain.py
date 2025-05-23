69144aa0c81e46bae746a5cad7b73accc948bc9b
Florian-BARRE <flo.others@gmail.com>
2025-05-23 18:00:59 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -102,7 +102,7 @@ class MainBrain(Brain):
         
         navigator.add_navigation_task(
             NavigatorTaskParams(
-                goal=OrientedPoint(50, 150, 0),
+                goal=OrientedPoint(50, 70, 0),
                 timeout=None,
                 path_planner_params=BasicPathPlannerParams(),
                 trajectory_planner_params=SequentialTrajectoryPlannerParams(),
