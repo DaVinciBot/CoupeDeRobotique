63bd46c5acaa3536c51f0cee3d03e6dd0673d923
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-25 16:21:22 +0200

    test

--- robot1/rasp/main.py
+++ robot1/rasp/main.py
@@ -118,8 +118,8 @@ if __name__ == "__main__":
 
     os.chdir("/home/dvb/CoupeDeRobotique/robot1/rasp")
     # Jack
-    jack = PIN(CONFIG.JACK_PIN)
-    jack.setup("input_pulldown", reverse_state=True)
+    # jack = PIN(CONFIG.JACK_PIN)
+    # jack.setup("input_pulldown", reverse_state=True)
 
     # Movement
     # Movement manager
@@ -138,7 +138,7 @@ if __name__ == "__main__":
         lidar=lidar,
         arena=arena,
         ws_cmd=ws_cmd,
-        jack=jack,
+        jack=None,
     )
 
     """
