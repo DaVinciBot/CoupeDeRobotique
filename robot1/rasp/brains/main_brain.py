7ccd3b0d2c389470f9a4b801ec6718d9a575cf65
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-25 16:02:11 +0200

    test

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -72,7 +72,7 @@ class MainBrain(Brain):
 
         self.jack = jack
         
-        self.PINS_TO_TEST = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+        self.PINS_TO_TEST = [2, 3, 4, 7, 8, 26]
         self.factory = LGPIOFactory()
 
         super().__init__(logger, self)
