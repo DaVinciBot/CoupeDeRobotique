075d70f88977aa7f74285aa293e3cff3affc614c
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2025-05-25 15:33:22 +0200

    fix

--- robot1/rasp/main.py
+++ robot1/rasp/main.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     os.chdir("/home/dvb/CoupeDeRobotique/robot1/rasp")
     # Jack
     jack = PIN(CONFIG.JACK_PIN)
-    jack.setup("input", reverse_state=True)
+    jack.setup("output", reverse_state=True)
 
     # Movement
     # Movement manager
