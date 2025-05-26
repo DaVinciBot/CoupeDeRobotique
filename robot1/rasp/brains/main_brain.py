e7f0a4485af2b55e759b0d35a1f5e7cd3a8e8602
Florian-BARRE <flo.others@gmail.com>
2025-05-26 20:38:57 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -119,7 +119,7 @@ class MainBrain(Brain):
                 ally_zone=self.arena.ally_zone,
                 enemy_zone=self.arena.enemy_zone,
             )
-            #rolling_basis.set_speed_and_position(*cmd.get_command())
+            rolling_basis.set_speed_and_position(*cmd.get_command())
         
   
         # yellow_strategy_runner.handle(
