6726e1d30bc3784260c34da38c614fc2f532a70c
Florian-BARRE <flo.others@gmail.com>
2025-05-26 19:43:50 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -101,8 +101,9 @@ class MainBrain(Brain):
         
         print("############## Starting Rolling Basis ##############")
         print("1 - Starting position: ", rolling_basis.odometrie)
+        print("2 - Target position: ", OrientedPoint(20, 40, 0))
         rolling_basis.set_odometrie(OrientedPoint(20, 20, 0))
-        print("2 - New position: ", rolling_basis.odometrie)
+        print("3 - New position: ", rolling_basis.odometrie)
     
         navigator.add_navigation_task(
             NavigatorTaskParams(
