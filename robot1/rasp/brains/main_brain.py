4808bd9b8dd881a1796016b89ab2ba18cd459e52
Florian-BARRE <flo.others@gmail.com>
2025-05-27 15:22:25 +0200

    e

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -218,7 +218,8 @@ class MainBrain(Brain):
         await self.wait_for_trigger()
         self.arena.set_team_color(TeamColor.YELLOW)
         # Start robot position
-        start_position = OrientedPoint(13, 17, 0)
+        #start_position = OrientedPoint(13, 17, 0)
+        start_position = OrientedPoint(0, 0, 0)
         self.arena.enemy_zone.update(
             self.arena.team_color, start_position, Point(290, 190)
         )
