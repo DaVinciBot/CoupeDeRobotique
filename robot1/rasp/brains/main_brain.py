95dc9e314e3f7c8abf84c2dfba6ddea737f43361
Florian-BARRE <flo.others@gmail.com>
2025-05-27 10:31:11 +0200

    fix

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -150,7 +150,10 @@ class MainBrain(Brain):
         fig, ax = plt.subplots()
 
         # --- MetaProg is insane (loop) --- #
-
+        lidar_points = self.remove_outside(
+            self._pol_to_abs_cart(self.lidar.scan_to_polars())
+        )
+        
         ax.clear()
         self.arena.visualize(
             # Visualization options
@@ -164,6 +167,7 @@ class MainBrain(Brain):
             # Additional options
             # additional_zones=[self.th_ally_zone],
             # additional_points=list(obstacles.geoms) if not is_empty(obstacles) else None,
+            additional_points=lidar_points,
         )
         plt.pause(0.01)
 
