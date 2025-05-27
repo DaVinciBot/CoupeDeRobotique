f20fc16e410760b000496983503ba39daf1abe9d
Florian-BARRE <flo.others@gmail.com>
2025-05-27 13:55:50 +0200

    F

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -109,6 +109,8 @@ class MainBrain(Brain):
             logger=Logger(identifier="RollingBasis", follow_logger_manager_rules=True)
         )
         time.sleep(1)
+        rolling_basis._initialize_pids()
+        time.sleep(1)
         rolling_basis.set_odometrie(self.rolling_basis_odometrie)
         time.sleep(1)
 
