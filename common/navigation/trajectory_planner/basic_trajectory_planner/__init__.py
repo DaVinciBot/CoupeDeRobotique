32dcbab669acb6be96e73de640bcd0214d8ff274
austin <eliottroussille@gmail.com>
2025-05-28 14:29:40 +0200

    fix

--- common/navigation/trajectory_planner/basic_trajectory_planner/__init__.py
+++
@@ -1,6 +0,0 @@
-from navigation.trajectory_planner.basic_trajectory_planner.basic_trajectory_planner import (
-    BasicTrajectoryPlanner,
-)
-from navigation.trajectory_planner.basic_trajectory_planner.basic_trajectory_planner_params import (
-    BasicTrajectoryPlannerParams,
-)
