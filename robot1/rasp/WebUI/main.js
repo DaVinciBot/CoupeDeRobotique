a8853437e475018fff8fa22a041b539bc322393d
austin <eliottroussille@gmail.com>
2025-05-28 19:49:23 +0200

    jack and bau state

--- robot1/rasp/WebUI/main.js
+++ robot1/rasp/WebUI/main.js
@@ -2,13 +2,34 @@ let console_div = document.querySelector('.log')
 
 let log = new WebSocketManager();
 log.add_ws("ui")
-log.add_handler( (event) => {
-    // parse JSON then display it
+log.add_handler((event) => {
+    // Parse the JSON message and display the console data
     let data = JSON.parse(event.data);
-    if (data.msg == "Msg received") {
-        show_console_data(data.data, true)
+    if (data.msg === "update ui data") {
+        show_console_data(JSON.stringify(data.data), true);
+
+        let jack_state = document.getElementById("Tirette");
+        let bau_state = document.getElementById("BAU");
+
+        jack_state.style.backgroundColor = data.data["Tirette"] ? "limegreen" : "red";
+        bau_state.style.backgroundColor = data.data["BAU"] ? "limegreen" : "red";
+
+        //TODO:PAMI
+        // for (let key in data.data) {
+        //     if (data.data.hasOwnProperty(key)) {
+        //         let element = document.getElementById(key);
+        //         if (element) {
+        //             // Si la valeur est un objet imbriqué, on l'affiche sous forme de chaîne JSON
+        //             if (typeof data.data[key] === 'object') {
+        //                 element.innerText = JSON.stringify(data.data[key]);
+        //             } else {
+        //                 element.innerText = data.data[key];
+        //             }
+        //         }
+        //     }
+        // }
     }
-})
+});
 
 function show_console_data(data, add = false) {
     if (add) {
