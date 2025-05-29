5796773cc6cc50ea9bfd5e45c0d4e166741f89fb
Florian-BARRE <flo.others@gmail.com>
2025-05-30 01:39:08 +0200

    a

--- robot1/rasp/controllers/rolling_basis/rolling_basis.py
+++ robot1/rasp/controllers/rolling_basis/rolling_basis.py
@@ -132,7 +132,7 @@ class RollingBasis(BaseComTeensy):
             + struct.pack("<d", target_position.y)
             + struct.pack("<d", target_position.theta)
         )
-        self.logger.info(f"SET: {target_position.x}, {target_position.y}, {target_position.theta}")
+        
         # Send the composed message to the Teensy
         # https://docs.python.org/3/library/struct.html#format-characters
         self.send_bytes(msg)
