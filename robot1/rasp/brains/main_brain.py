838960c275a2cc18e5a71371dc0e6981589c3146
Florian-BARRE <flo.others@gmail.com>
2025-05-29 17:22:57 +0200

    f

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -293,7 +293,7 @@ class MainBrain(Brain):
     @Brain.task(process=False, run_on_start=True)
     async def start(self):
         # 1. Wait for the team color to be set
-        self.arena.set_team_color(TeamColor.YELLOW)
+        self.arena.set_team_color(TeamColor.BLUE)
         #await self.wait_for_team()
 
         # 2. Define the starting position based on the team color
