1b1325f8c8f5cce7cfb0bf0f7866b4a55a289eb0
austin <eliottroussille@gmail.com>
2025-05-29 08:11:48 +0200

    f

--- common/arena/show_arena/arena.py
+++ common/arena/show_arena/arena.py
@@ -306,7 +306,7 @@ class ShowArena(BaseArena):
                 logger=blue_reserved_zone_logger,
                 buffer_size=obstacle_buffer,
                 polygon=create_straight_rectangle(Point((15, 155)), Point((60, 200))),
-                go_to_positions=[Point(37.5, 177.5)],
+                go_to_positions=[Point(37.5, 177.5, pi / 2)],
             )
         )
 
@@ -315,7 +315,7 @@ class ShowArena(BaseArena):
                 logger=yellow_reserved_zone_logger,
                 buffer_size=obstacle_buffer,
                 polygon=create_straight_rectangle(Point((285, 155)), Point((240, 200))),
-                go_to_positions=[Point(262.5, 177.5)],
+                go_to_positions=[Point(262.5, 177.5, pi / 2)],
             )
         )
 
