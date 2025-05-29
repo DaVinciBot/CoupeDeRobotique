62015d208b8eb4c59ec7e330e0264daeb1df2e65
austin <eliottroussille@gmail.com>
2025-05-29 09:52:59 +0200

    f

--- robot1/rasp/boombot_strategy/sub_graphs/pickup.py
+++ robot1/rasp/boombot_strategy/sub_graphs/pickup.py
@@ -74,7 +74,7 @@ def get_pickup_sub_graph(zone_pickup_id: int, ctx: ShowGameContext) -> BaseSubGr
         f"[Pickup] go to take stuff {zone_pickup_id}",
         BaseTaskNode(
             name=f"[Pickup] go to take stuff {zone_pickup_id}",
-            tasks=PreciseForward(15),
+            tasks=PreciseForward(16),
             scoring_function=DefaultScoringFunction(),
         ),
     )
