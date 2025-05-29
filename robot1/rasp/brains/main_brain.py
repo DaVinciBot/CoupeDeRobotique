10f960606b9d07c3cee916f8c5df2c7862255724
Florian-BARRE <flo.others@gmail.com>
2025-05-29 22:18:18 +0200

    a

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -91,7 +91,7 @@ class MainBrain(Brain):
             NavigatorTaskParams(
                 goal=None,
                 timeout=None,
-                path_planner_params=DeltaPathPlannerParams(rotation=pi, distance=20),
+                path_planner_params=DeltaPathPlannerParams(distance=20),
                 trajectory_planner_params=SequentialTrajectoryPlannerParams(),
                 speed_profiler=CONFIG.ROLLING_BASIS_DEFAULT_SPEED_PROFILER,
                 avoidance_params=NoAvoidanceParams(),
