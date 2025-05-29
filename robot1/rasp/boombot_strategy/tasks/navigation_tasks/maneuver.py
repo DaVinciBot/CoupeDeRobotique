8831f8be2edf5e63b9a209ec0ba3a2e57a637bf7
gaelAphack <gael.le_mouel@edu.devinci.fr>
2025-05-29 11:10:35 +0200

    Change SpeedProfile for Pickup and Movement

--- robot1/rasp/boombot_strategy/tasks/navigation_tasks/maneuver.py
+++ robot1/rasp/boombot_strategy/tasks/navigation_tasks/maneuver.py
@@ -35,7 +35,7 @@ class PreciseForward(NavigationTask):
             goal=None,
             path_planner_params=DeltaPathPlannerParams(distance=distance),
             trajectory_planner_params=SequentialTrajectoryPlannerParams(),
-            speed_profiler=CONFIG.ROLLING_BASIS_SLOW_SPEED_PROFILER,
+            speed_profiler=CONFIG.ROLLING_BASIS_DEFAULT_SPEED_PROFILER,
             avoidance_params=NoAvoidanceParams(),
             acs_detection_profile_params=NoAcsDetectionProfileParams(),
         )
