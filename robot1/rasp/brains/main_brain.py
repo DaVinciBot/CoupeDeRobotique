899e06cce020b68e5aae67b3def69812fe291c13
Florian-BARRE <flo.others@gmail.com>
2025-05-29 05:23:50 +0200

    a

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -248,14 +248,16 @@ class MainBrain(Brain):
     @Brain.task(process=False, run_on_start=False)
     async def wait_for_team(self):
         while self.arena.team_color == TeamColor.UNDEFINED:
-            self.logger.info(
-                f"Team color is set to {self.arena.team_color.name.lower()}. Starting the brain."
-            )
+            await asyncio.sleep(0.1)
+
+        self.logger.info(
+            f"Team color is set to {self.arena.team_color.name.lower()}. Starting the brain."
+        )
 
     @Brain.task(process=False, run_on_start=True)
     async def start(self):
         # 1. Wait for the team color to be set
-        #self.arena.set_team_color(TeamColor.YELLOW)
+        # self.arena.set_team_color(TeamColor.YELLOW)
         await self.wait_for_team()
 
         # 2. Define the starting position based on the team color
