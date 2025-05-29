0d6060f3ee44a451614d8e235a31cf2130e3c5e8
Florian-BARRE <flo.others@gmail.com>
2025-05-30 01:48:33 +0200

    increase delay of stabilisation ! Safe mode

--- robot1/rasp/boombot_strategy/tasks/navigation_tasks/go_to_color_reserved_zone.py
+++ robot1/rasp/boombot_strategy/tasks/navigation_tasks/go_to_color_reserved_zone.py
@@ -22,7 +22,7 @@ class GoToColorReservedZoneToFinishGame(NavigationTask):
             acs_detection_profile_params=RectangularProjectionAcsDetectionProfileParams(
                 acs_distance=50, width_view=40
             ),
-            stabilization_delay=0.5
+            stabilization_delay=2
         )
 
 
@@ -37,5 +37,5 @@ class GoToColorReservedZoneToConstruct(NavigationTask):
             acs_detection_profile_params=RectangularProjectionAcsDetectionProfileParams(
                 acs_distance=40, width_view=40
             ),
-            stabilization_delay=2,  # Delay to stabilize before construction
+            stabilization_delay=4,  # Delay to stabilize before construction
         )
