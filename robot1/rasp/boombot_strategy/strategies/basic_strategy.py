5c7d122144fdaf8ac47e672020d4f20e36a74ea3
austin <eliottroussille@gmail.com>
2025-05-30 15:48:43 +0200

    implement auto transitions

--- robot1/rasp/boombot_strategy/strategies/basic_strategy.py
+++ robot1/rasp/boombot_strategy/strategies/basic_strategy.py
@@ -77,25 +77,6 @@ class BasicStrategy(BaseStrategy):
             go_to_backstage,
         )
 
-        # deploy_banner_subgraph.get_exits()[0].add_transition(
-        #     DirectTransition(first_pickup_subgraph.get_entry())
-        # )
-        # first_pickup_subgraph.get_exits()[0].add_transition(
-        #     DirectTransition(first_construct_subgraph.get_entry())
-        # )
-        # first_construct_subgraph.get_exits()[0].add_transition(
-        #     DirectTransition(second_pickup_subgraph.get_entry())
-        # )
-        # second_pickup_subgraph.get_exits()[0].add_transition(
-        #     DirectTransition(second_construct_subgraph.get_entry())
-        # )
-        # second_construct_subgraph.get_exits()[0].add_transition(
-        #     DirectTransition(go_to_backstage)
-        # )
-        # first_construct_subgraph.get_exits()[0].add_transition(
-        #     DirectTransition(go_to_backstage)
-        # )
-
         # Create the graph runner starting from the first subgraph
         self.runner = GraphRunner(
             logger=Logger(
--- robot1/rasp/boombot_strategy/strategies/tower_rush_strategy.py
+++ robot1/rasp/boombot_strategy/strategies/tower_rush_strategy.py
@@ -72,17 +72,12 @@ class TowerRushStrategy(BaseStrategy):
         )
 
         # Connect the subgraphs in execution order
-        deploy_banner_subgraph.get_exits()[0].add_transition(
-            DirectTransition(first_pickup_subgraph.get_entry())
-        )
-        first_pickup_subgraph.get_exits()[0].add_transition(
-            DirectTransition(first_construct_subgraph.get_entry())
-        )
-        first_construct_subgraph.get_exits()[0].add_transition(
-            DirectTransition(second_pickup_subgraph.get_entry())
-        )
-        second_pickup_subgraph.get_exits()[0].add_transition(
-            DirectTransition(go_to_backstage)
+        self._auto_build_transitions(
+            deploy_banner_subgraph,
+            first_pickup_subgraph,
+            first_construct_subgraph,
+            second_pickup_subgraph,
+            go_to_backstage,
         )
 
         # Create the graph runner starting from the first subgraph
