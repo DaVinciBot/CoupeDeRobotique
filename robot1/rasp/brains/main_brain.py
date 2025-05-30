6da0f9f3e976a765783b0c0d589febe126c0fded
Florian-BARRE <flo.others@gmail.com>
2025-05-30 10:59:52 +0200

    tets yellow

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -280,7 +280,7 @@ class MainBrain(Brain):
 
     @Brain.task(process=False, run_on_start=True)
     async def start(self):
-        self.arena.set_team_color(TeamColor.BLUE)
+        self.arena.set_team_color(TeamColor.YELLOW)
         await self.wait_for_team()
 
         start_position = OrientedPoint(0, 0, 0)
