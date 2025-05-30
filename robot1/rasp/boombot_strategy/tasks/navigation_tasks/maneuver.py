a005ea9029c82e346262d201835e9b7d8992af97
Florian-BARRE <flo.others@gmail.com>
2025-05-30 11:17:44 +0200

    fix add tehta to target centroid

--- robot1/rasp/boombot_strategy/tasks/navigation_tasks/maneuver.py
+++ robot1/rasp/boombot_strategy/tasks/navigation_tasks/maneuver.py
@@ -28,6 +28,8 @@ from navigation import (
     NavigatorTask,
 )
 
+from geometry import OrientedPoint, Point
+
 class RelativeBackward(NavigationTask):
     def __init__(self, distance: float):
         super().__init__(
@@ -75,11 +77,16 @@ class GoCentroidOfZone(NavigationTask):
     def _initialize(self, ctx: BaseGameContext) -> None:
         self._is_initialized = True
 
+        go_to_position: OrientedPoint = ctx.arena.compute_goal_position(self.goal)
+        centroid: Point = ctx.arena.zones[self.zone_id].polygon.centroid
+        centroid_with_theta: OrientedPoint = OrientedPoint(
+            centroid.x, centroid.y,
+            go_to_position.theta
+        )
+
         self.navigator_task: NavigatorTask = NavigatorTask(
             params=NavigatorTaskParams(
-                goal=(
-                    ctx.arena.zones[self.zone_id].polygon.centroid
-                ),  # goal can be None when we use DeltaPathPlanner
+                goal=centroid_with_theta,
                 timeout=self.timeout,
                 path_planner_params=self.path_planner_params,
                 trajectory_planner_params=self.trajectory_planner_params,
