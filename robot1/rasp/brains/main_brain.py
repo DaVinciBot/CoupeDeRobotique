58c4cf302fdcfc8346dc54ee4e58ba16045f4d75
Clément <skaos@mythik.fr>
2025-10-30 15:03:31 +0100

    🚑 set odometry to 0

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -184,6 +184,7 @@ class MainBrain(Brain):
                     score=self.score,
                 ),
             )
+        rolling_basis.set_odometrie(OrientedPoint(0, 0, 0))
 
         self.should_send_start = True
         self.status = "starting"
