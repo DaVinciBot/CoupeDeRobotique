768ca91dd26f63ef8f629f59404ab0ed286c5b0a
austin <eliottroussille@gmail.com>
2025-11-12 14:33:16 +0100

    🚑 rollback generic gamecontext in odometrie tasks

--- robot1/rasp/boombot_strategy/tasks/navigation_tasks/odometrie.py
+++ robot1/rasp/boombot_strategy/tasks/navigation_tasks/odometrie.py
@@ -4,12 +4,12 @@ from __future__ import annotations
 
 from typing import override
 
+from boombot_strategy.winter_game_context import WinterGameContext
 from geometry import OrientedPoint
-from strategy.core.base_game_context import BaseGameContext
 from strategy.core.tasks.base_task import BaseTask
 
 
-class SetOdometrie[GameContextT: BaseGameContext](BaseTask[GameContextT]):
+class SetOdometrie(BaseTask[WinterGameContext]):
     """Task to update the robot's odometry position.
 
     The task uses the provided ``x``, ``y`` and ``theta`` values if given;
@@ -37,11 +37,11 @@ class SetOdometrie[GameContextT: BaseGameContext](BaseTask[GameContextT]):
         super().__init__()
 
     @override
-    def handle(self, ctx: GameContextT) -> bool:
+    def handle(self, ctx: WinterGameContext) -> bool:
         """Handle the execution of the odometry setting task.
 
         Args:
-            ctx (GameContextT): Context containing game state and arena info.
+            ctx (WinterGameContext): Context containing game state and arena info.
 
         Returns:
             bool: Always returns ``True`` after setting the new odometry.
@@ -70,15 +70,15 @@ class SetOdometrie[GameContextT: BaseGameContext](BaseTask[GameContextT]):
         return True
 
 
-class WallSetOdometrie[GameContextT: BaseGameContext](BaseTask[GameContextT]):
+class WallSetOdometrie(BaseTask[WinterGameContext]):
     """Task to reset odometry based on the closest wall goal at runtime."""
 
     @override
-    def handle(self, ctx: GameContextT) -> bool:
+    def handle(self, ctx: WinterGameContext) -> bool:
         """Handle the execution of the wall-based odometry reset task.
 
         Args:
-            ctx (GameContextT): Context containing game state and arena info.
+            ctx (WinterGameContext): Context containing game state and arena info.
 
         Returns:
             bool: Returns SetOdometrie task result or True if no wall goal found.
