6f2e0187ac45d4ae3b8fbd7ca58731528cfdee27
austin <eliottroussille@gmail.com>
2025-12-22 11:39:07 +0100

    test

--- robot1/rasp/a_config_loader/config_loader.py
+++ robot1/rasp/a_config_loader/config_loader.py
@@ -357,7 +357,7 @@ class CONFIG:
             **ROLLING_BASIS_SPEED_PROFILES_LINEAR["slow"],
         ),
         angular_speed_profile=BasicSpeedProfile(
-            ROLLING_BASIS_SPEED_PROFILES_ANGULAR["slow"]["speed"],
+            20.0,
         ),
     )
     ROLLING_BASIS_SPEED_PROFILER_PID: SpeedProfiler = SpeedProfiler(
