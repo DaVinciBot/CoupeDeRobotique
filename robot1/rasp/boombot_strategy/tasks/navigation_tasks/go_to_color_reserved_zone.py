78d14cd58449553c71346bea2a08746293e52c4a
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2026-01-09 13:14:01 +0100

    implement pathfinder in go to reserved zone
    lucy

--- robot1/rasp/boombot_strategy/tasks/navigation_tasks/go_to_color_reserved_zone.py
+++ robot1/rasp/boombot_strategy/tasks/navigation_tasks/go_to_color_reserved_zone.py
@@ -4,28 +4,48 @@ from __future__ import annotations
 
 from a_config_loader import CONFIG
 from boombot_strategy.tasks.navigation_tasks.navigation_task import NavigationTask
+from pathfinding.core.grid import Grid
+
+from geometry import OrientedPoint
 from navigation.avoidance.acs_detection_profiles.rectangular_projection_acs_detection_profile import (  # noqa: E501
     RectangularProjectionAcsDetectionProfileParams,
 )
 from navigation.avoidance.stop_and_wait_avoidance import StopAndWaitAvoidanceParams
 from navigation.path_planner.basic_path_planner import BasicPathPlannerParams
+from navigation.path_planner.astar_path_planner import AStarPathPlannerParams
 from navigation.trajectory_planner.sequential_trajectory_planner import (
     SequentialTrajectoryPlannerParams,
 )
+from navigation.path_planner.structs import Direction
+from boombot_strategy.winter_game_context import WinterGameContext as GameContext
 
 
 class GoToColorReservedZoneToFinishGame(NavigationTask):
     """Task to navigate to a color reserved zone to finish the game."""
 
-    def __init__(self, color_reserved_zone_id: int) -> None:
+    def __init__(self, color_reserved_zone_id: int, ctx: GameContext) -> None:
         """Initialize the GoToColorReservedZoneToFinishGame task.
 
         Args:
             color_reserved_zone_id (int): The ID of the target color reserved zone.
         """
+
+        grid = ctx.arena.grid_manager.get_static_and_dynamic_grid()
+        goal = ctx.arena.compute_goal_position(1)
+        # goal = ctx.arena.compute_goal_position(color_reserved_zone_id)
+
+        self.path_planner_params = AStarPathPlannerParams(
+            grid=grid,
+            path_resolution=10,
+            chunk_size=20,
+            start=ctx.arena.ally_zone.point,
+            goal=goal,
+            direction=Direction.FORWARD,
+        )
+
         super().__init__(
-            goal=color_reserved_zone_id,
-            path_planner_params=BasicPathPlannerParams(),
+            goal=goal,
+            path_planner_params=self.path_planner_params,
             trajectory_planner_params=SequentialTrajectoryPlannerParams(),
             speed_profiler=CONFIG.ROLLING_BASIS_DEFAULT_SPEED_PROFILER,
             avoidance_params=StopAndWaitAvoidanceParams(timeout=30),
--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -16,7 +16,7 @@ from ws_comms import WServerRouteManager, WSmsg
 from a_config_loader import CONFIG
 from arena.base_arena import TeamColor
 from boombot_strategy import WinterGameContext
-from boombot_strategy.strategies import TowerRushAltStrategy
+from boombot_strategy.strategies import TowerRushAltStrategy, OnlyBannerStrategy
 from controllers.actuators import ActuatorsShow, ActuatorsShowDummy
 from controllers.rolling_basis import RollingBasis, RollingBasisDummy
 from geometry import OrientedPoint
@@ -142,7 +142,7 @@ class MainBrain(Brain):
 
         # --- 3) Build the strategy --- #
 
-        strategy = TowerRushAltStrategy(
+        strategy = OnlyBannerStrategy(
             WinterGameContext(
                 arena=self.arena,
                 rolling_basis=rolling_basis,
