00df6508e8a1e9c34f4ed524d8e433efc2d81052
Clément <skaos@mythik.fr>
2026-02-19 12:06:18 +0100

    add a time

--- jetson/src/lora/lora.py
+++ jetson/src/lora/lora.py
@@ -52,6 +52,7 @@ class LoRa:
         serial_data = data.encode("utf-8")
         if self.serial and self.serial.is_open:
             self.serial.write(serial_data)
+            time.sleep(0.1)
         else:
             print(
                 "Erreur: Le module LoRa n'est pas connecté.",
