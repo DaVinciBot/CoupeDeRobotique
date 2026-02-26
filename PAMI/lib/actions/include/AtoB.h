7f34d59a87168a4401f6fb87bd63de0236085aa1
austin <eliottroussille@gmail.com>
2026-02-26 14:45:45 +0100

    add pami actions

---
+++ PAMI/lib/actions/include/AtoB.h
@@ -0,0 +1,26 @@
+#ifndef ACTIONS_ATOB_H
+#define ACTIONS_ATOB_H
+
+#include "action.h"
+#include "rolling_basis.h"
+
+class AtoB : public Action {
+   public:
+    AtoB(RollingBasis* rb, const Point& target);
+    ~AtoB() = default;
+    void start() override;
+    void update() override;
+    void stop() override;
+    bool isFinished() const override;
+    const char* name() const override;
+
+   private:
+    RollingBasis* _rb;
+    Point _target;
+    unsigned long _startMs = 0;
+    unsigned long _timeoutMs = 15000;  // Ms
+    float _arriveTolMm = 5.0f;
+    bool _finished = false;
+};
+
+#endif
