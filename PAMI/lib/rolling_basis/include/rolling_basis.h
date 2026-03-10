e7cf0868274b29c61d14df8b22eb3aa2b0d3cbb5
austin <eliottroussille@gmail.com>
2026-03-10 01:08:12 +0100

    revert mimicou commit

--- PAMI/lib/rolling_basis/include/rolling_basis.h
+++ PAMI/lib/rolling_basis/include/rolling_basis.h
@@ -128,18 +128,6 @@ class RollingBasis {
      * @return float angular speed in radians per second
      */
     float getAngularSpeedRadPerS() const;
-    /**
-     * @brief defined the speed for the two motors
-     *
-     * @return an avancement (?)
-     */
-    void setSpeed(float linearMmPerS, float angularRadPerS);
-    /**
-     * @brief update the motors
-     *
-     * @return do the update
-     */
-    void updateMotors();
 
    private:
     // void _computeOdometry(float dt);
--- PAMI/lib/rolling_basis/src/rolling_basis.cpp
+++ PAMI/lib/rolling_basis/src/rolling_basis.cpp
@@ -137,11 +137,3 @@ float RollingBasis::getLinearSpeedMmPerS() const {
 float RollingBasis::getAngularSpeedRadPerS() const {
     return _angularSpeed;
 }
-void RollingBasis::setSpeed(float linearMmPerS, float angularRadPerS) {
-    _sendWheelSpeeds(linearMmPerS, angularRadPerS);
-}
-
-void RollingBasis::updateMotors() {
-    _leftMotor->update();
-    _rightMotor->update();
-}
