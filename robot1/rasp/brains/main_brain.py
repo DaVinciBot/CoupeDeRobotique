4317f8411f7a7d5b3e6f2f26c49ed815ebb587d9
Faye Anne-Marie <anne-marie.faye@edu.devinci.fr>
2026-04-09 19:04:38 +0200

    a

--- robot1/rasp/brains/main_brain.py
+++ robot1/rasp/brains/main_brain.py
@@ -201,6 +201,7 @@ class MainBrain(Brain):
         # --- MetaProg is insane (loop) --- #
 
         lora.receive()
+        self.logger.error("dans la bouvle")
         # sc.receive_data()
         self.shared_crates = _crates_to_dict(sc.crates)
 
