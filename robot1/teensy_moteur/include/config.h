d6029dc3f6e9c1664a9d934c531792fa30dd866a
austin <eliottroussille@gmail.com>
2026-05-07 18:06:28 +0200

    refactor: Correct LEFT_WHEEL_DIAMETER and RIGHT_WHEEL_DIAMETER values for accurate calculations

--- robot1/teensy_moteur/include/config.h
+++ robot1/teensy_moteur/include/config.h
@@ -29,8 +29,8 @@
 // Encoder
 #define ENCODER_RESOLUTION 1024
 #define ENTRAXE 30.9
-#define LEFT_WHEEL_DIAMETER 6.1
-#define RIGHT_WHEEL_DIAMETER 6.016
+#define LEFT_WHEEL_DIAMETER 6.154
+#define RIGHT_WHEEL_DIAMETER 6.051
 
 // PIDs (position supervision)
 #define KP_LINEAR_POSITION 0.8
