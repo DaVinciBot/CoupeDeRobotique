36a10f148d43b8d4f5471fbdf4f669a17858d87e
austin <eliottroussille@gmail.com>
2026-05-15 17:13:42 +0200

    refactor: Adjust movement distances and delay parameters in GoBackstageStrategy for improved navigation

--- robot1/rasp/boombot_strategy/strategies/go_backstage_strategy.py
+++ robot1/rasp/boombot_strategy/strategies/go_backstage_strategy.py
@@ -19,7 +19,7 @@ if TYPE_CHECKING:
 
 POSITION_TOLERANCE_CM = 0.9
 ANGLE_TOLERANCE_RAD = 0.15
-FINISH_AFTER_EXPECTED_END_DELAY_S = 10.0
+FINISH_AFTER_EXPECTED_END_DELAY_S = 15.0
 
 
 class GoBackstageStrategy(BaseStrategy):
@@ -41,7 +41,7 @@ class GoBackstageStrategy(BaseStrategy):
         forward = BaseTaskNode(
             name="Move Forward",
             tasks=RelativeForward(
-                distance=105.0,
+                distance=85.0,
                 position_reached_tolerance_cm=POSITION_TOLERANCE_CM,
                 angle_reached_tolerance_rad=ANGLE_TOLERANCE_RAD,
                 finish_after_expected_end_delay_s=FINISH_AFTER_EXPECTED_END_DELAY_S,
@@ -71,7 +71,7 @@ class GoBackstageStrategy(BaseStrategy):
         turn2 = BaseTaskNode(
             name="Turn 2",
             tasks=RelativeRotation(
-                angle=rotation_sign * 8 * math.pi / 18,
+                angle=rotation_sign * math.pi / 2,
                 position_reached_tolerance_cm=POSITION_TOLERANCE_CM,
                 angle_reached_tolerance_rad=0.05,
                 finish_after_expected_end_delay_s=20.0,
@@ -81,7 +81,7 @@ class GoBackstageStrategy(BaseStrategy):
         end = BaseTaskNode(
             name="End",
             tasks=RelativeForward(
-                distance=105.0,
+                distance=95.0,
                 position_reached_tolerance_cm=POSITION_TOLERANCE_CM,
                 angle_reached_tolerance_rad=ANGLE_TOLERANCE_RAD,
                 finish_after_expected_end_delay_s=80.0,
