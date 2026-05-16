e128e2732577e4e748113552247011fcdbc4fad9
austin <eliottroussille@gmail.com>
2026-05-16 09:31:40 +0200

    refactor: Adjust movement parameters in GoBackstageStrategy for improved navigation and add position reset task

--- robot1/rasp/boombot_strategy/strategies/go_backstage_strategy.py
+++ robot1/rasp/boombot_strategy/strategies/go_backstage_strategy.py
@@ -12,6 +12,7 @@ from boombot_strategy.tasks.navigation_tasks.maneuver import (
     RelativeForward,
     RelativeRotation,
 )
+from boombot_strategy.tasks.navigation_tasks.odometrie import SetOdometrie
 from log_manager import LogLogger
 from strategy.core import GraphRunner
 from strategy.core.task_nodes import BaseTaskNode
@@ -23,7 +24,7 @@ if TYPE_CHECKING:
 POSITION_TOLERANCE_CM = 0.9
 ANGLE_TOLERANCE_RAD = 0.05
 FINISH_AFTER_EXPECTED_END_DELAY_S = 5.0
-END_START_DELAY_S = 93.0
+END_START_DELAY_S = 90.0
 
 
 class _StrategyTimer:
@@ -131,17 +132,21 @@ class GoBackstageStrategy(BaseStrategy):
             ],
         )
 
-        backward_20 = make_backward("Backward 20 cm", 20.0)
+        backward_20 = make_backward("Backward 20 cm", 24.0)
         turn_90_1 = make_turn("Turn 90 deg 1", math.pi / 2)
-        forward_52 = make_forward("Forward 52 cm", 55.0)
+        forward_52 = make_forward("Forward 52 cm", 35.0)
         turn_minus_90_1 = make_turn("Turn -90 deg 1", -math.pi / 2)
         forward_90 = make_forward("Forward 96 cm", 110.0)
-        backward_20_2 = make_backward("Backward 20 cm", 9.0)
+        reset_position = BaseTaskNode(
+            name="Reset Position", tasks=SetOdometrie(0, 0, 0)
+        )
+        backward_20_2 = make_backward("Backward 20 cm", 8.5)
         turn_90_2 = make_turn("Turn 90 deg 2", math.pi / 2)
         forward_58_3 = make_forward("Forward 58.3 cm", 63.0)
-        backward_22 = make_backward("Backward 22 cm", 95.0)
+        backward_22 = make_backward("Backward 22 cm", 85.0)
+        turn_90_3 = make_turn("Turn 90 deg 3", math.pi / 2)
         forward_55 = make_forward("Forward 55 cm", 55.0)
-        turn_90_3 = make_turn("Turn 90 deg 3", 0.14)
+        turn_90_4 = make_turn("Turn 90 deg 3", 0.14)
         forward_100 = make_forward("Forward 100 cm", 100.0)
 
         wait_before_end = BaseTaskNode(
@@ -157,12 +162,14 @@ class GoBackstageStrategy(BaseStrategy):
             forward_52,
             turn_minus_90_1,
             forward_90,
+            reset_position,
             backward_20_2,
             turn_90_2,
             forward_58_3,
             backward_22,
-            forward_55,
             turn_90_3,
+            forward_55,
+            turn_90_4,
             wait_before_end,
             forward_100,
         )
